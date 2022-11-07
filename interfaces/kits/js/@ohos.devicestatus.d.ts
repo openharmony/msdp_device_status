3eebb8b4ccbdcfb74a466a574a53a2e786c39744
hhh2 <pengfei.hou@huawei.com>
2022-11-07 14:11:17 +0800

    Modify @ohos.devicestatus.d.ts Signed-off-by:hhh2<pengfei.hou@huawei.com>
    Signed-off-by: hhh2 <pengfei.hou@huawei.com>

--- interfaces/kits/js/@ohos.devicestatus.d.ts
+++ interfaces/kits/js/@ohos.devicestatus.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,200 +13,107 @@
  * limitations under the License.
  */
 
-import { AsyncCallback } from "./basic";
+import { Callback } from "./basic";
 
 /**
- * 订阅用户设备状态通知
+ * Declares a namespace that provides APIs to report the device status.
  *
  * @since 9
  * @syscap SystemCapability.Msdp.DeviceStatus
- * @import import sensor from '@ohos.DeviceStatus'
- * @permission N/A
+ * @import import DeviceStatus from '@ohos.DeviceStatus'
  */
-declare namespace DeviceStatus {
+declare namespace deviceStatus {
     /**
-     * 行为识别数据。
+     * Declares a response interface to receive the device status.
+     *
      * @syscap SystemCapability.Msdp.DeviceStatus
+     * @since 9
      */
-    export interface ActivityResponse {
-        eventType: EventType
-    }
-	
-    /**
-     * 绝对静止的数据。
-     * @syscap SystemCapability.Msdp.DeviceStatus
-     */
-    export interface StillResponse extends ActivityResponse {}
-    
-    /**
-     * 相对静止的数据。
-     * @syscap SystemCapability.Msdp.DeviceStatus
-     */
-    export interface RelativeStillResponse extends ActivityResponse {}
-    
-    /**
-     * 水平位置的数据。
-     * @syscap SystemCapability.Msdp.DeviceStatus
-     */
-    export interface VerticalPositionResponse extends ActivityResponse {}
-    
-    /**
-     * 垂直位置的数据。
-     * @syscap SystemCapability.Msdp.DeviceStatus
-     */
-    export interface HorizontalPositionResponse extends ActivityResponse {}
-	
-    /**
-     * 行为识别类型。
-     * @syscap SystemCapability.Msdp.DeviceStatus
-     */
-    export enum ActivityType {
-        TYPE_STILL = "still",
-        TYPE_RELATIVE_STILL = "relativeStill",
-        TYPE_VERTICAL_POSITION = "verticalPosition",
-        TYPE_HORIZONTAL_POSITION = "horizontalPosition"
+    interface ActivityResponse {
+        state: ActivityState;
     }
 
     /**
-     * 事件类型。
+     * Declares the device status type.
+     *
      * @syscap SystemCapability.Msdp.DeviceStatus
+     * @since 9
      */
-    export enum EventType {
+    type ActivityType = 'still' | 'relativeStill' | 'horizontalPosition' | 'verticalPosition';
+
+    /**
+     * Enumerates the device status events.
+     *
+     * @syscap SystemCapability.Msdp.DeviceStatus
+     * @since 9
+     */
+    enum ActivityEvent {
+        /**
+         * Event indicating entering device status.
+         */ 
         ENTER = 1,
+
+        /**
+         * Event indicating exiting device status.
+         */
         EXIT = 2,
+
+        /**
+         * Event indicating entering and exiting device status.
+         */
         ENTER_EXIT = 3
     }
 
+    /**
+     * Declares a response interface to receive the device status.
+     *
+     * @syscap SystemCapability.Msdp.DeviceStatus
+     * @since 9
+     */
+    enum ActivityState {
+        /**
+         * Entering device status.
+         */
+        ENTER = 1,
+
+        /**
+         * Exiting device status.
+         */
+        EXIT = 2
+    }
+
     /**	
-     * 订阅绝对静止。
+     * Subscribes to the device status.
      *
+     * @param activity Indicates the device status type. For details, see {@code type: ActivityType}.
+     * @param event Indicates the device status event.
+     * @param reportLatencyNs Indicates the event reporting period.
+     * @param callback Indicates the callback for receiving reported data.
+     * @syscap SystemCapability.Msdp.DeviceStatus
      * @since 9
-     * @param type 订阅绝对静止, {@code type: ActivityType.TYPE_STILL}.
-     * @param eventType enter and exit event.
-     * @param reportLatencyNs report event latency.
-     * @param callback callback function, receive reported data.
      */
-    function on(type: ActivityType.TYPE_STILL, eventType: EventType, reportLatencyNs: number, callback: AsyncCallback<StillResponse>): void;
-	
+    function on(activity: ActivityType, event: ActivityEvent, reportLatencyNs: number, callback: Callback<ActivityResponse>): void;
+
     /**
-     * 订阅相对静止。
+     * Obtains the device status.
      *
+     * @param activity Indicates the device status type. For details, see {@code type: ActivityType}.
+     * @param callback Indicates the callback for receiving reported data.
+     * @syscap SystemCapability.Msdp.DeviceStatus
      * @since 9
-     * @param type 订阅相对静止, {@code type: ActivityType.TYPE_RELATIVE_STILL}.
-     * @param eventType enter and exit event.
-     * @param reportLatencyNs report event latency.
-     * @param callback callback function, receive reported data.
      */
-    function on(type: ActivityType.TYPE_RELATIVE_STILL, eventType: EventType, reportLatencyNs: number, callback: AsyncCallback<RelativeStillResponse>): void;
-	
+    function once(activity: ActivityType, callback: Callback<ActivityResponse>): void;
+
     /**
-     * 订阅水平位置。
+     * Unsubscribes from the device status.
      *
+     * @param activity Indicates the device status type. For details, see {@code type: ActivityType}.
+     * @param event Indicates the device status event.
+     * @param callback Indicates the callback for receiving reported data.
+     * @syscap SystemCapability.Msdp.DeviceStatus
      * @since 9
-     * @param type 订阅水平位置, {@code type: ActivityType.TYPE_VERTICAL_POSITION}.
-     * @param eventType enter and exit event.
-     * @param reportLatencyNs report event latency.
-     * @param callback callback function, receive reported data.
      */
-    function on(type: ActivityType.TYPE_VERTICAL_POSITION, eventType: EventType, reportLatencyNs: number, callback: AsyncCallback<VerticalPositionResponse>): void;
-	
-    /**
-     * 订阅垂直位置。
-     *
-     * @since 9
-     * @param type 订阅垂直位置, {@code type: ActivityType.TYPE_HORIZONTAL_POSITION}.
-     * @param eventType enter and exit event.
-     * @param reportLatencyNs report event latency.
-     * @param callback callback function, receive reported data.
-     */
-    function on(type: ActivityType.TYPE_HORIZONTAL_POSITION, eventType: EventType, reportLatencyNs: number, callback: AsyncCallback<HorizontalPositionResponse>): void;
-     
-    /**
-     * 查询是否绝对静止。
-     *
-     * @since 9
-     * @param type 查询是否绝对静止, {@code type: ActivityType.TYPE_STILL}.
-     * @param eventType enter and exit event.
-     * @param reportLatencyNs report event latency.
-     * @param callback callback function, receive reported data.
-     */
-    function once(type: ActivityType.TYPE_STILL, callback: AsyncCallback<StillResponse>): void;
-	
-    /**
-     * 查询是否相对静止。
-     *
-     * @since 9
-     * @param type 查询是否相对静止, {@code type: ActivityType.TYPE_RELATIVE_STILL}.
-     * @param eventType enter and exit event.
-     * @param reportLatencyNs report event latency.
-     * @param callback callback function, receive reported data.
-     */
-    function once(type: ActivityType.TYPE_RELATIVE_STILL, callback: AsyncCallback<RelativeStillResponse>): void;
-	
-    /**
-     * 查询是否水平位置。
-     *
-     * @since 9
-     * @param type 查询是否水平位置, {@code type: ActivityType.TYPE_VERTICAL_POSITION}.
-     * @param eventType enter and exit event.
-     * @param reportLatencyNs report event latency.
-     * @param callback callback function, receive reported data.
-     */
-    function once(type: ActivityType.TYPE_VERTICAL_POSITION, callback: AsyncCallback<VerticalPositionResponse>): void;
-	
-    /**
-     * 查询是否垂直位置。
-     *
-     * @since 9
-     * @param type 查询是否垂直位置, {@code type: ActivityType.TYPE_HORIZONTAL_POSITION}.
-     * @param eventType enter and exit event.
-     * @param reportLatencyNs report event latency.
-     * @param callback callback function, receive reported data.
-     */
-    function once(type: ActivityType.TYPE_HORIZONTAL_POSITION, callback: AsyncCallback<HorizontalPositionResponse>): void;
-	
-    /**
-     * 取消订阅绝对静止。
-     *
-     * @since 9
-     * @param type 查询是否绝对静止, {@code type: ActivityType.TYPE_STILL}.
-     * @param eventType enter and exit event.
-     * @param reportLatencyNs report event latency.
-     * @param callback callback function, receive reported data.
-     */
-    function off(type: ActivityType.TYPE_STILL, eventType: EventType, callback?: AsyncCallback<void>): void;
-    
-    /**
-     * 取消订阅相对静止。
-     *
-     * @since 9
-     * @param type 查询是否相对静止, {@code type: ActivityType.TYPE_RELATIVE_STILL}.
-     * @param eventType enter and exit event.
-     * @param reportLatencyNs report event latency.
-     * @param callback callback function, receive reported data.
-     */
-    function off(type: ActivityType.TYPE_RELATIVE_STILL, eventType: EventType, callback?: AsyncCallback<void>): void;
-    
-    /**
-     * 取消订阅水平位置。
-     *
-     * @since 9
-     * @param type 查询是否水平位置, {@code type: ActivityType.TYPE_VERTICAL_POSITION}.
-     * @param eventType enter and exit event.
-     * @param reportLatencyNs report event latency.
-     * @param callback callback function, receive reported data.
-     */
-    function off(type: ActivityType.TYPE_VERTICAL_POSITION, eventType: EventType, callback?: AsyncCallback<void>): void;
-    
-    /**
-     * 取消订阅垂直位置。
-     *
-     * @since 9
-     * @param type 查询是否垂直位置, {@code type: ActivityType.TYPE_HORIZONTAL_POSITION}.
-     * @param eventType enter and exit event.
-     * @param reportLatencyNs report event latency.
-     * @param callback callback function, receive reported data.
-     */
-    function off(type: ActivityType.TYPE_HORIZONTAL_POSITION, eventType: EventType, callback?: AsyncCallback<void>): void;
+    function off(activity: ActivityType, event: ActivityEvent, callback?: Callback<ActivityResponse>): void;
 }
-export default DeviceStatus;
+
+export default deviceStatus;
