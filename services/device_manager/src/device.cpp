24eaf7f16e802a75a26f418e4e1227850c8077d2
hhh2 <pengfei.hou@huawei.com>
2022-11-29 09:22:19 +0800

    code transplant: device manager
    Signed-off-by: hhh2 <pengfei.hou@huawei.com>
    Change-Id: Iebe274b976306726b24cfa8d30afe920f6642be6
    Signed-off-by: hhh2 <pengfei.hou@huawei.com>

---
+++ services/device_manager/src/device.cpp
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "device.h"
+
+#include <regex>
+
+#include <openssl/sha.h>
+
+#include "fi_log.h"
+#include "napi_constants.h"
+#include "util.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+namespace {
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL { LOG_CORE, MSDP_DOMAIN_ID, "Device" };
+const std::string INPUT_VIRTUAL_DEVICE_NAME { "DistributedInput " };
+const std::string SPLIT_SYMBOL { "|" };
+const std::string DH_ID_PREFIX { "Input_" };
+} // namespace
+
+Device::Device(std::shared_ptr<::OHOS::MMI::InputDevice> inputDev)
+    : inputDev_(inputDev)
+{
+    Populate();
+}
+
+void Device::Populate()
+{
+#ifdef OHOS_BUILD_ENABLE_COORDINATION
+    if (IsRemote()) {
+        networkId_ = MakeNetworkId(GetPhys());
+    }
+    dhid_ = GenerateDescriptor();
+#endif // OHOS_BUILD_ENABLE_COORDINATION
+
+    if (IsKeyboard()) {
+        int32_t ret = InputMgr->GetKeyboardType(GetId(),
+            std::bind(&Device::onKeyboardTypeObtained, this, std::placeholders::_1));
+        if (ret != 0) {
+            FI_HILOGE("GetKeyboardType failed");
+        }
+    }
+}
+
+#ifdef OHOS_BUILD_ENABLE_COORDINATION
+
+bool Device::IsRemote() const
+{
+    return (GetName().find(INPUT_VIRTUAL_DEVICE_NAME) != std::string::npos);
+}
+
+std::string Device::MakeNetworkId(const std::string &phys) const
+{
+    std::vector<std::string> strList;
+    StringSplit(phys, SPLIT_SYMBOL, strList);
+    if (strList.size() == 3) {
+        return strList[1];
+    }
+    return EMPTYSTR;
+}
+
+std::string Device::GenerateDescriptor() const
+{
+    const std::string phys = GetPhys();
+    std::string descriptor;
+    if (IsRemote() && !phys.empty()) {
+        FI_HILOGI("physicalPath:%{public}s", phys.c_str());
+        std::vector<std::string> strList;
+        StringSplit(phys.c_str(), SPLIT_SYMBOL, strList);
+        if (strList.size() == 3) {
+            descriptor = strList[2];
+        }
+        return descriptor;
+    }
+
+    int32_t vendor = GetVendor();
+    const std::string name = GetName();
+    const std::string uniq = GetUniq();
+    int32_t product = GetProduct();
+    std::string rawDescriptor = StringPrintf(":%04x:%04x:", vendor, product);
+
+    // add handling for USB devices to not unique kbs that show up twice
+    if (!uniq.empty()) {
+        rawDescriptor += "uniqueId:" + uniq;
+    }
+    if (!phys.empty()) {
+        rawDescriptor += "physicalPath:" + phys;
+    }
+    if (!name.empty()) {
+        rawDescriptor += "name:" + std::regex_replace(name, std::regex(" "), "");
+    }
+    descriptor = DH_ID_PREFIX + Sha256(rawDescriptor);
+    FI_HILOGI("Created descriptor raw: %{public}s", rawDescriptor.c_str());
+    return descriptor;
+}
+
+std::string Device::Sha256(const std::string &in) const
+{
+    unsigned char out[SHA256_DIGEST_LENGTH * 2 + 1] = {0};
+    SHA256_CTX ctx;
+    SHA256_Init(&ctx);
+    SHA256_Update(&ctx, in.data(), in.size());
+    SHA256_Final(&out[SHA256_DIGEST_LENGTH], &ctx);
+    // here we translate sha256 hash to hexadecimal. each 8-bit char will be presented by two characters([0-9a-f])
+    constexpr int32_t WIDTH = 4;
+    constexpr unsigned char MASK = 0x0F;
+    const char* hexCode = "0123456789abcdef";
+    constexpr int32_t DOUBLE_TIMES = 2;
+    for (int32_t i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+        unsigned char value = out[SHA256_DIGEST_LENGTH + i];
+        // uint8_t is 2 digits in hexadecimal.
+        out[i * DOUBLE_TIMES] = hexCode[(value >> WIDTH) & MASK];
+        out[i * DOUBLE_TIMES + 1] = hexCode[value & MASK];
+    }
+    out[SHA256_DIGEST_LENGTH * DOUBLE_TIMES] = 0;
+    return reinterpret_cast<char*>(out);
+}
+
+#endif // OHOS_BUILD_ENABLE_COORDINATION
+
+void Device::onKeyboardTypeObtained(int32_t keyboardType)
+{
+    if (keyboardType >= ::OHOS::MMI::KEYBOARD_TYPE_NONE &&
+        keyboardType < ::OHOS::MMI::KEYBOARD_TYPE_MAX) {
+        keyboardType_ = static_cast<::OHOS::MMI::KeyboardType>(keyboardType);
+    } else {
+        keyboardType_ = ::OHOS::MMI::KEYBOARD_TYPE_NONE;
+    }
+}
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
---
+++ services/device_manager/src/device_manager.cpp
@@ -0,0 +1,221 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "device_manager.h"
+
+#include <linux/input.h>
+
+#include "devicestatus_define.h"
+#include "fi_log.h"
+#include "input_device_cooperate_util.h"
+#include "napi_constants.h"
+#include "util.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+namespace {
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL { LOG_CORE, MSDP_DOMAIN_ID, "DeviceManager" };
+} // namespace
+
+DeviceManager::DeviceManager() {}
+DeviceManager::~DeviceManager() {}
+
+int32_t DeviceManager::Enable()
+{
+    CALL_INFO_TRACE;
+    int32_t ret = InputMgr->RegisterDevListener(CHANGED_TYPE, shared_from_this());
+    if (ret != RET_OK) {
+        FI_HILOGE("RegisterDevListener failed");
+        return ret;
+    }
+
+    ret = InputMgr->GetDeviceIds(
+        [this](std::vector<int32_t> &deviceIds) {
+            for (const int32_t id : deviceIds) {
+                int32_t ret = InputMgr->GetDevice(id,
+                    std::bind(&DeviceManager::OnDeviceInfoObtained, this, std::placeholders::_1));
+                if (ret != 0) {
+                    FI_HILOGE("GetDevice failed");
+                }
+            }
+        }
+    );
+    if (ret != RET_OK) {
+        FI_HILOGW("GetDeviceIds failed");
+        return ret;
+    }
+    return RET_OK;
+}
+
+void DeviceManager::Disable()
+{
+    CALL_INFO_TRACE;
+    int32_t ret = InputMgr->UnregisterDevListener(CHANGED_TYPE, shared_from_this());
+    if (ret != RET_OK) {
+        FI_HILOGE("UnregisterDevListener failed");
+        return;
+    }
+
+    devices_.clear();
+}
+
+void DeviceManager::OnDeviceAdded(int32_t deviceId, const std::string &type)
+{
+    CALL_INFO_TRACE;
+    int32_t ret = InputMgr->GetDevice(deviceId,
+        std::bind(&DeviceManager::OnDeviceInfoObtained, this, std::placeholders::_1));
+    if (ret != 0) {
+        FI_HILOGE("GetDevice failed");
+    }
+}
+
+void DeviceManager::OnDeviceRemoved(int32_t deviceId, const std::string &type)
+{
+    CALL_INFO_TRACE;
+    if (auto devIter = devices_.find(deviceId); devIter != devices_.cend()) {
+        FI_HILOGI("Device(\'%{public}s\') removed", devIter->second->GetName().c_str());
+        devices_.erase(devIter);
+    }
+}
+
+void DeviceManager::OnDeviceInfoObtained(std::shared_ptr<::OHOS::MMI::InputDevice> inputDev)
+{
+    CALL_INFO_TRACE;
+    CHKPV(inputDev);
+    std::shared_ptr<Device> dev = std::make_shared<Device>(inputDev);
+    auto [devIter, isOk] = devices_.insert_or_assign(dev->GetId(), dev);
+    if (!isOk) {
+        FI_HILOGW("Device(\'%{public}s\') exists already", devIter->second->GetName().c_str());
+    }
+}
+
+std::shared_ptr<Device> DeviceManager::GetDevice(int32_t id) const
+{
+    if (auto devIter = devices_.find(id); devIter != devices_.cend()) {
+        return devIter->second;
+    }
+    return nullptr;
+}
+
+void DeviceManager::Dump(int fd, const std::vector<std::string> &args)
+{
+    CALL_DEBUG_ENTER;
+}
+
+#ifdef OHOS_BUILD_ENABLE_COORDINATION
+
+bool DeviceManager::IsRemote(int32_t id) const
+{
+    if (auto devIter = devices_.find(id); devIter != devices_.end()) {
+        return devIter->second->IsRemote();
+    }
+    return false;
+}
+
+std::vector<std::string> DeviceManager::GetCooperateDhids(int32_t deviceId)
+{
+    std::vector<std::string> dhids;
+    auto devIter = devices_.find(deviceId);
+    if (devIter == devices_.end()) {
+        FI_HILOGI("Find pointer id failed");
+        return dhids;
+    }
+    std::shared_ptr<Device> dev = devIter->second;
+    if (!dev->IsPointerDevice()) {
+        FI_HILOGI("Not pointer device");
+        return dhids;
+    }
+    dhids.push_back(dev->GetDhid());
+    FI_HILOGI("unq: %{public}s, type:%{public}s", dhids.back().c_str(), "pointer");
+
+    const std::string localNetworkId { COOPERATE::GetLocalDeviceId() };
+    const auto pointerNetworkId { dev->IsRemote() ? dev->GetNetworkId() : localNetworkId };
+
+    for (const auto &[id, devPtr]: devices_) {
+        const auto networkId { devPtr->IsRemote() ? devPtr->GetNetworkId() : localNetworkId };
+        if (networkId != pointerNetworkId) {
+            continue;
+        }
+        if (devPtr->GetKeyboardType() == KEYBOARD_TYPE_ALPHABETICKEYBOARD) {
+            dhids.push_back(devPtr->GetDhid());
+            FI_HILOGI("unq: %{public}s, type:%{public}s", dhids.back().c_str(), "supportkey");
+        }
+    }
+    return dhids;
+}
+
+std::vector<std::string> DeviceManager::GetCooperateDhids(const std::string &dhid)
+{
+    int32_t inputDeviceId { -1 };
+    for (const auto &[id, dev] : devices_) {
+        if (dev->GetDhid() == dhid) {
+            inputDeviceId = id;
+            break;
+        }
+    }
+    return GetCooperateDhids(inputDeviceId);
+}
+
+std::string DeviceManager::GetOriginNetworkId(int32_t id)
+{
+    auto devIter = devices_.find(id);
+    if (devIter == devices_.end()) {
+        FI_HILOGE("Failed to search for the device: id %{public}d", id);
+        return EMPTYSTR;
+    }
+    auto networkId = devIter->second->GetNetworkId();
+    if (networkId.empty()) {
+        networkId = COOPERATE::GetLocalDeviceId();
+    }
+    return networkId;
+}
+
+std::string DeviceManager::GetOriginNetworkId(const std::string &dhid)
+{
+    if (dhid.empty()) {
+        return EMPTYSTR;
+    }
+    for (const auto &[id, dev] : devices_) {
+        if (dev->IsRemote() && dev->GetDhid() == dhid) {
+            return dev->GetNetworkId();
+        }
+    }
+    return EMPTYSTR;
+}
+
+std::string DeviceManager::GetDhid(int32_t deviceId) const
+{
+    auto devIter = devices_.find(deviceId);
+    if (devIter != devices_.end()) {
+        return devIter->second->GetDhid();
+    }
+    return EMPTYSTR;
+}
+
+bool DeviceManager::HasLocalPointerDevice() const
+{
+    for (const auto &[id, dev] : devices_) {
+        if (!dev->IsRemote() && dev->IsPointerDevice()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+#endif // OHOS_BUILD_ENABLE_COORDINATION
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
