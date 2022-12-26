d9af8a139fd675bd49cee114cab9bf15da64ceef
mayunteng_1 <mayunteng@huawei.com>
2022-12-26 14:05:38 +0800

    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: I5dfd6688c6076cbb7ed08b7ab03a99d6ccd993a7

---
+++ test/fuzztest/getinputdevicecooperatestate_fuzzer/getinputdevicecooperatestate_fuzzer.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "getinputdevicecooperatestate_fuzzer.h"
+
+#include "securec.h"
+
+#include "interaction_manager.h"
+#include "fi_log.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+namespace {
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MSDP_DOMAIN_ID, "GetInputDeviceCooperateStateFuzzTest" };
+} // namespace
+
+void GetInputDeviceCooperateStateFuzzTest(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return;
+    }
+    const std::string deviceId(reinterpret_cast<const char*>(data), size);
+    auto fun = [](bool inputdevice) {
+        FI_HILOGD("Get inputdevice state success");
+    };
+    InteractionManager::GetInstance()->GetInputDeviceCoordinationState(deviceId, fun);
+}
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t * data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Msdp::DeviceStatus::GetInputDeviceCooperateStateFuzzTest(data, size);
+    return 0;
+}
---
+++ test/fuzztest/startinputdevicecooperate_fuzzer/startinputdevicecooperate_fuzzer.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "startinputdevicecooperate_fuzzer.h"
+
+#include "coordination_message.h"
+#include "interaction_manager.h"
+#include "fi_log.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+namespace {
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MSDP_DOMAIN_ID, "StartInputDeviceCooperateFuzzTest" };
+} // namespace
+
+void StartInputDeviceCooperateFuzzTest(const uint8_t* data, size_t  size)
+{
+    if (data == nullptr) {
+        return;
+    }
+    const std::string sinkDeviceId(reinterpret_cast<const char*>(data), size);
+    const int32_t srcInputDeviceId = *(reinterpret_cast<const int32_t*>(data));
+    auto fun = [](std::string listener, CoordinationMessage cooperateMessages) {
+        FI_HILOGD("StartInputDeviceCooperateFuzzTest");
+    };
+    InteractionManager::GetInstance()->StartInputDeviceCoordination(sinkDeviceId, srcInputDeviceId, fun);
+}
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return 0;
+    }
+    if (size < sizeof(int32_t)) {
+        return 0;
+    }
+    OHOS::Msdp::DeviceStatus::StartInputDeviceCooperateFuzzTest(data, size);
+    return 0;
+}
+
