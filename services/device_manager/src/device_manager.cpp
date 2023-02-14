df93d6732ea5cbca2d3d35b8e6064f67d34b8af8
mayunteng_1 <mayunteng@huawei.com>
2023-02-14 08:42:38 +0000

    remove unused interfaces
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: Ib1c6946ff827b0bd7bf5758cad25ac8025b5a399
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>

--- services/device_manager/src/device_manager.cpp
+++ services/device_manager/src/device_manager.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -26,7 +26,9 @@
 #include "devicestatus_define.h"
 #include "fi_log.h"
 #include "device.h"
+#ifdef OHOS_BUILD_ENABLE_COORDINATION
 #include "coordination_util.h"
+#endif // OHOS_BUILD_ENABLE_COORDINATION
 #include "napi_constants.h"
 
 namespace OHOS {
