8c7c2e116fc27a6c486d934fc484744d71666ae2
wuliangdong <wuliangdong1@huawei.com>
2023-03-09 09:27:57 +0000

    addFuzz
    Signed-off-by: wuliangdong <wuliangdong1@huawei.com>
    Change-Id: I5479ca8a978fc10e9ca7629e5753dd35374d63aa

---
+++ test/fuzztest/startdrag_fuzzer/start_drag_fuzzer.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "start_drag_fuzzer.h"
+
+#include <memory>
+
+#include "securec.h"
+
+#include "devicestatus_define.h"
+#include "drag_data.h"
+#include "fi_log.h"
+#include "interaction_manager.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+namespace {
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MSDP_DOMAIN_ID, "StartDragFuzzTest" };
+} // namespace
+
+
+size_t GetObject(const uint8_t *data, size_t size, T &object)
+{
+    size_t objectSize = sizeof(object);
+    if (objectSize > size) {
+        return 0;
+    }
+    errno_t ret = memcpy_s(&object, objectSize, data, objectSize);
+    if (ret != RET_OK) {
+        return 0;
+    }
+    return objectSize;
+}
+
+std::shared_ptr<Media::PixelMap> CreatePixelMap(int32_t width, int32_t height)
+{
+    CALL_DEBUG_ENTER;
+    if (width <= 0 || width > MAX_PIXEL_MAP_WIDTH ||
+       height <= 0 || height > MAX_PIXEL_MAP_HEIGHT) {
+        FI_HILOGE("Invalid size,width:%{public}d,height:%{public}d", width, height);
+        return nullptr;
+    }
+    OHOS::Media::InitializationOptions opts;
+    opts.size.width = width;
+    opts.size.height = height;
+    std::unique_ptr<Media::PixelMap> uniquePixelMap = Media::PixelMap::Create(opts);
+    std::shared_ptr<Media::PixelMap> pixelMap = std::move(uniquePixelMap);
+    return pixelMap;
+}
+
+DragData CreateDragData(const uint8_t* data, size_t size)
+{
+    size_t startPos = 0;
+    PictureResourse pictureResourse;
+    startPos += GetObject<int32_t>(data + startPos, size - startPos, pictureResourse.x);
+    startPos += GetObject<int32_t>(data + startPos, size - startPos, pictureResourse.y);
+    int32_t width = 0;
+    int32_t height = 0;
+    startPos += GetObject<int32_t>(data + startPos, size - startPos, width);
+    startPos += GetObject<int32_t>(data + startPos, size - startPos, height);
+    pictureResourse.pixelMap = CreatePixelMap(width, height);
+    
+    DragData dragData;
+    startPos += GetObject<int32_t>(data + startPos, size - startPos, dragData.sourceType);
+    startPos += GetObject<int32_t>(data + startPos, size - startPos, dragData.dragNum);
+    startPos += GetObject<int32_t>(data + startPos, size - startPos, dragData.pointerId);
+    startPos += GetObject<int32_t>(data + startPos, size - startPos, dragData.displayX);
+    startPos += GetObject<int32_t>(data + startPos, size - startPos, dragData.displayY);
+    startPos += GetObject<int32_t>(data + startPos, size - startPos, dragData.displayId);
+    dragData.pictureResourse = pictureResourse;
+    return dragData;
+}
+
+void StartDragFuzzTest(const uint8_t* data, size_t  size)
+{
+    if (data == nullptr) {
+        return;
+    }
+    auto func = [](const DragNotifyMsg& notifyMessage) {
+        FI_HILOGD("StartDragFuzzTest:displayX:%{public}d, displayY:%{public}d, result:%{public}d, target:%{public}d",
+            notifyMessage.displayX, notifyMessage.displayY, notifyMessage.result, notifyMessage.targetPid);
+    };
+    DragData dragData = CreateDragData(data, size);
+    InteractionManager::GetInstance()->StartDrag(dragData, func);
+    InteractionManager::GetInstance()->StopDrag(0);
+}
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return 0;
+    }
+    if (size < sizeof(int32_t)) {
+        return 0;
+    }
+    OHOS::Msdp::DeviceStatus::StartDragFuzzTest(data, size);
+    return 0;
+}
+
---
+++ test/fuzztest/stopdrag_fuzzer/stop_drag_fuzzer.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "stop_drag_fuzzer.h"
+
+#include "fi_log.h"
+#include "interaction_manager.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+namespace {
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MSDP_DOMAIN_ID, "StopDragFuzzTest" };
+} // namespace
+
+void StopDragFuzzTest(const uint8_t* data, size_t  size)
+{
+    if (data == nullptr) {
+        return;
+    }
+    int32_t result = *(reinterpret_cast<const int32_t*>(data));
+    InteractionManager::GetInstance()->StopDrag(result);
+}
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return 0;
+    }
+    if (size < sizeof(int32_t)) {
+        return 0;
+    }
+    OHOS::Msdp::DeviceStatus::StopDragFuzzTest(data, size);
+    return 0;
+}
+
