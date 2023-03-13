b1449d1892fcad630ca736ce5316d61ca9cb9472
mayunteng_1 <mayunteng@huawei.com>
2023-03-13 08:01:23 +0000

    add interface setdragwindowvisible
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: I17c0be9fffe07d079e78f3d08e7df0befa03adaf

--- services/interaction/drag/src/drag_manager.cpp
+++ services/interaction/drag/src/drag_manager.cpp
@@ -231,7 +231,6 @@ int32_t DragManager::OnStopDrag()
 
 int32_t DragManager::OnSetDragWindowVisible(bool visible)
 {
-    CALL_DEBUG_ENTER;
     visible_ = visible;
     FI_HILOGD("set drag window visible %{public}s success", visible ? "true" : "false");
     return RET_OK;
