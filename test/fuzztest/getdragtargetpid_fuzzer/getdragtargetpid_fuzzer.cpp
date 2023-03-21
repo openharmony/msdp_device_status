9b462833deec34651d7edff8c94acc626ebea2ae
Justin_Hu <hugongcheng@huawei.com>
2023-03-21 13:16:29 +0000

    getdragtargetpid fuzzer
    Signed-off-by: Justin_Hu <hugongcheng@huawei.com>
    Change-Id: I9138f8de8705aed5c9a38af730d601aee40cc3c4

---
+++ test/fuzztest/getdragtargetpid_fuzzer/getdragtargetpid_fuzzer.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "getdragtargetpid_fuzzer.h"
+
+#include "interaction_manager.h"
+#include "fi_log.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+namespace {
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MSDP_DOMAIN_ID, "GetDragTargetPidFuzzTest" };
+} // namespace
+
+void GetDragTargetPidFuzzTest(const uint8_t* data, size_t size)
+{
+    FI_HILOGD("GGG GetDragTargetPidFuzzTest");
+    InteractionManager::GetInstance()->GetDragTargetPid();
+}
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+    if (size < sizeof(int32_t)) {
+        return 0;
+    }
+    OHOS::Msdp::DeviceStatus::GetDragTargetPidFuzzTest(data, size);
+    return 0;
+}
