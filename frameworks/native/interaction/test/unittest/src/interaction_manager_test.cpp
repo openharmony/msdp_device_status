69de65dd9dafc695637977cef0c4070337a16d29
wuliangdong <wuliangdong1@huawei.com>
2023-04-13 12:35:06 +0000

    fixUT
    Signed-off-by: wuliangdong <wuliangdong1@huawei.com>
    Change-Id: I8448e8dbedb36f1b351d19923b68e46d03b51af3

--- frameworks/native/interaction/test/unittest/src/interaction_manager_test.cpp
+++ frameworks/native/interaction/test/unittest/src/interaction_manager_test.cpp
@@ -12,9 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include <future>
 #include <optional>
-#include <thread>
 #include <utility>
 #include <vector>
 
