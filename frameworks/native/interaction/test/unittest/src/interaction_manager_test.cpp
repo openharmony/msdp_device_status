ef0bd2037b296acb612aaf9104cb6241b5007b7a
wuliangdong <wuliangdong1@huawei.com>
2023-04-26 02:57:37 +0000

    fix UT
    Signed-off-by: wuliangdong <wuliangdong1@huawei.com>
    Change-Id: Ib994d0647c9521f72135524856c8503f1c87c9b2

--- frameworks/native/interaction/test/unittest/src/interaction_manager_test.cpp
+++ frameworks/native/interaction/test/unittest/src/interaction_manager_test.cpp
@@ -24,7 +24,6 @@
 
 #include <gtest/gtest.h>
 #include "image_type.h"
-#include "image_source.h"
 #include "input_device.h"
 #include "input_manager.h"
 #include "pointer_event.h"
@@ -40,7 +39,6 @@ namespace DeviceStatus {
 using namespace testing::ext;
 namespace {
 constexpr OHOS::HiviewDFX::HiLogLabel LABEL = { LOG_CORE, MSDP_DOMAIN_ID, "InteractionManagerTest" };
-std::string CURSOR_DRAG_PATH {"/system/etc/device_status/drag_icon/File_Drag.png"};
 constexpr int32_t TIME_WAIT_FOR_OP_MS { 100 };
 constexpr int32_t TIME_WAIT_FOR_INJECT_MS { 20 };
 constexpr int32_t TIME_WAIT_FOR_TOUCH_DOWN_MS { 1000 };
@@ -52,6 +50,7 @@ constexpr int32_t DRAG_SRC_Y { 0 };
 constexpr int32_t DRAG_DST_X { 800 };
 constexpr int32_t DRAG_DST_Y { 800 };
 constexpr int32_t DRAG_NUM { 1 };
+constexpr uint32_t DEFAULT_ICON_COLOR { 0x0000FFFF };
 constexpr bool HAS_CANCELED_ANIMATION { true };
 constexpr bool HAS_CUSTOM_ANIMATION { true };
 constexpr int32_t MOVE_STEP { 5 };
@@ -70,8 +69,7 @@ public:
     static std::shared_ptr<MMI::InputDevice> GetDevice(int32_t deviceId);
     static std::pair<int32_t, int32_t> GetMouseAndTouch();
     static void PrintPixelMap(std::shared_ptr<Media::PixelMap> pixelMap);
-    static void SetPixelMap(std::shared_ptr<Media::PixelMap> pixelMap);
-    std::shared_ptr<Media::PixelMap> CreatePixelMap(int32_t pixelMapWidth, int32_t pixelMapHeight);
+    std::shared_ptr<Media::PixelMap> CreatePixelMap(int32_t width, int32_t height);
     std::optional<DragData> CreateDragData(std::pair<int32_t, int32_t> pixelMapSize, int32_t sourceType,
     int32_t pointerId, int32_t displayId, std::pair<int32_t, int32_t> location);
     MMI::PointerEvent::PointerItem CreatePointerItem(int32_t pointerId,
@@ -146,54 +144,30 @@ void InteractionManagerTest::TearDown()
     std::this_thread::sleep_for(std::chrono::milliseconds(TIME_WAIT_FOR_OP_MS));
 }
 
-std::shared_ptr<OHOS::Media::PixelMap> InteractionManagerTest::CreatePixelMap(int32_t pixelMapWidth, int32_t pixelMapHeight)
+std::shared_ptr<Media::PixelMap> InteractionManagerTest::CreatePixelMap(int32_t width, int32_t height)
 {
-    std::string imagePath = CURSOR_DRAG_PATH;
-    OHOS::Media::SourceOptions opts;
-    opts.formatHint = "image/png";
-    uint32_t errCode = 0;
-    auto imageSource = OHOS::Media::ImageSource::CreateImageSource(imagePath, opts, errCode);
-    if (imageSource == nullptr) {
+    CALL_DEBUG_ENTER;
+    if (width <= 0 || width > MAX_PIXEL_MAP_WIDTH ||
+       height <= 0 || height > MAX_PIXEL_MAP_HEIGHT) {
+        FI_HILOGE("Invalid size,width:%{public}d,height:%{public}d", width, height);
         return nullptr;
     }
-    std::set<std::string> formats;
-    imageSource->GetSupportedFormats(formats);
-    OHOS::Media::DecodeOptions decodeOpts;
-    decodeOpts.desiredSize = {
-        .width = pixelMapWidth,
-        .height = pixelMapHeight
-    };
-    decodeOpts.allocatorType = OHOS::Media::AllocatorType::SHARE_MEM_ALLOC;
-    std::shared_ptr<OHOS::Media::PixelMap> pixelMap = imageSource->CreatePixelMap(decodeOpts, errCode);
-    PrintPixelMap(pixelMap);
+    OHOS::Media::InitializationOptions opts;
+    opts.size.width = width;
+    opts.size.height = height;
+    opts.pixelFormat = Media::PixelFormat::BGRA_8888;
+    opts.alphaType = Media::AlphaType::IMAGE_ALPHA_TYPE_OPAQUE;
+    opts.scaleMode = Media::ScaleMode::FIT_TARGET_SIZE;
+
+    int32_t colorLen = width * height;
+    uint32_t *color = new uint32_t[colorLen];
+    for (int32_t i = 0; i < colorLen; i++) {
+        color[i] = DEFAULT_ICON_COLOR;
+    }
+    std::shared_ptr<Media::PixelMap> pixelMap = Media::PixelMap::Create(color, colorLen, opts);
+    delete[] color;
     return pixelMap;
 }
-
-// std::shared_ptr<Media::PixelMap> InteractionManagerTest::CreatePixelMap(int32_t width, int32_t height)
-// {
-//     CALL_DEBUG_ENTER;
-//     if (width <= 0 || width > MAX_PIXEL_MAP_WIDTH ||
-//        height <= 0 || height > MAX_PIXEL_MAP_HEIGHT) {
-//         FI_HILOGE("Invalid size,width:%{public}d,height:%{public}d", width, height);
-//         return nullptr;
-//     }
-//     OHOS::Media::InitializationOptions opts;
-//     opts.size.width = width;
-//     opts.size.height = height;
-//     opts.pixelFormat = Media::PixelFormat::ARGB_8888;
-//     opts.alphaType = Media::AlphaType::IMAGE_ALPHA_TYPE_OPAQUE;
-//     int32_t colorLen = width * height;
-//     uint32_t *color = new uint32_t[colorLen];
-//     for (int32_t i = 0; i < colorLen; i++) {
-//         color[i] = 0xFF0000FF;
-//     }
-//     std::shared_ptr<Media::PixelMap> pixelMap = Media::PixelMap::Create(color, colorLen, opts);
-//     delete[] color;
-//     CHKPP(pixelMap);
-//     PrintPixelMap(pixelMap);
-//     SetPixelMap(pixelMap);
-//     return pixelMap;
-// }
 
 std::optional<DragData> InteractionManagerTest::CreateDragData(std::pair<int32_t, int32_t> pixelMapSize,
     int32_t sourceType, int32_t pointerId, int32_t displayId, std::pair<int32_t, int32_t> location)
@@ -217,28 +191,7 @@ std::optional<DragData> InteractionManagerTest::CreateDragData(std::pair<int32_t
     dragData.displayY = location.second;
     dragData.displayId = displayId;
     dragData.hasCanceledAnimation = HAS_CANCELED_ANIMATION;
-    PrintPixelMap(dragData.shadowInfo.pixelMap);
     return dragData;
-}
-
-void InteractionManagerTest::SetPixelMap(std::shared_ptr<Media::PixelMap> pixelMap)
-{
-    CALL_DEBUG_ENTER;
-    int32_t width = pixelMap->GetWidth();
-    int32_t height = pixelMap->GetHeight();
-    int32_t rowBytes = pixelMap->GetRowBytes();
-    int32_t pixelBytes = pixelMap->GetPixelBytes();
-    const uint8_t *pixels = pixelMap->GetPixels();
-    uint8_t *mutPixels = const_cast<uint8_t*> (pixels);
-    const uint8_t pixelColor[] = {0, 255, 0, 0};
-    for (int32_t i = 0; i < height; i++) {
-        for (int32_t j = 0; j < width; j++) {
-            for (int32_t k = 0; k < pixelBytes; k++) {
-                mutPixels[i * rowBytes + j * pixelBytes + k] = pixelColor[k];
-            }
-        }
-        std::cout << std::endl;
-    }
 }
 
 void InteractionManagerTest::PrintPixelMap(std::shared_ptr<Media::PixelMap> pixelMap)
@@ -331,7 +284,6 @@ void InteractionManagerTest::SimulateMoveEvent(std::pair<int, int> srcLocation, 
         FI_HILOGD("TEST:sourceType:%{public}d, pointerId:%{public}d, pointerAction:%{public}d",
             pointerEvent->GetSourceType(), pointerEvent->GetPointerId(), pointerEvent->GetPointerAction());
         INPUT_MANAGER->SimulateInputEvent(pointerEvent);
-        InteractionManager::GetInstance()->SetDragWindowVisible(true);
         std::this_thread::sleep_for(std::chrono::milliseconds(TIME_WAIT_FOR_INJECT_MS));
     }
 }
@@ -631,8 +583,6 @@ HWTEST_F(InteractionManagerTest, InteractionManagerTest_StartDrag_Mouse, TestSiz
             MMI::PointerEvent::SOURCE_TYPE_MOUSE, MOUSE_POINTER_ID, DISPLAY_ID, { DRAG_SRC_X, DRAG_SRC_Y });
         ASSERT_TRUE(dragData);
         int32_t ret = InteractionManager::GetInstance()->StartDrag(dragData.value(), callback);
-        ASSERT_EQ(ret, RET_OK);
-        ret = InteractionManager::GetInstance()->SetDragWindowVisible(true);
         ASSERT_EQ(ret, RET_OK);
         SimulateMoveEvent({ DRAG_SRC_X, DRAG_SRC_Y }, { DRAG_DST_X, DRAG_DST_Y },
             MMI::PointerEvent::SOURCE_TYPE_MOUSE, MOUSE_POINTER_ID, true);
