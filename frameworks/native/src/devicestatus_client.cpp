922edfba9c6ab2b3e8f3ffe139d4be307db57023
mayunteng <mayunteng@huawei.com>
2023-05-23 21:08:20 +0800

    Fix implicit conversion problems that may exist in the project code
    Signed-off-by: mayunteng <mayunteng@huawei.com>

--- frameworks/native/src/devicestatus_client.cpp
+++ frameworks/native/src/devicestatus_client.cpp
@@ -121,7 +121,7 @@ int32_t DeviceStatusClient::SubscribeCallback(Type type, ActivityEvent event, Re
     if (!ret) {
         DEV_HILOGW(SERVICE, "Insert pair to typeMap_ failed");
     }
-    DEV_HILOGD(INNERKIT, "typeMap_ %{public}d, type:%{public}d", typeMap_[type], type);
+    DEV_HILOGD(INNERKIT, "typeMap_:%{public}d, type:%{public}d", typeMap_[type], type);
     if (callback == nullptr) {
         DEV_HILOGE(SERVICE, "callback is nullptr");
         return RET_ERR;
