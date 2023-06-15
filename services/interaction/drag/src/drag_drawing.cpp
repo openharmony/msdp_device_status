b211ab9b9b16f0ddb7d5ccccac3d41dc73e8ee63
xing-jiangpan <xingjiangpan2@huawei.com>
2023-06-15 12:46:55 +0000

    The interface is added to update shadow picture
    Signed-off-by: xing-jiangpan <xingjiangpan2@huawei.com>
    Change-Id: Ic53e62f5a1c8fb756cb4126dbfe4fc6cdba120b4

--- services/interaction/drag/src/drag_drawing.cpp
+++ services/interaction/drag/src/drag_drawing.cpp
@@ -236,17 +236,17 @@ int32_t DragDrawing::UpdateShadowPic(const ShadowInfo &shadowInfo)
     CALL_DEBUG_ENTER;
     CHKPR(shadowInfo.pixelMap, RET_ERR);
     g_drawingInfo.pixelMap = shadowInfo.pixelMap;
+    Draw(g_drawingInfo.displayId, g_drawingInfo.displayX + shadowInfo.x - g_drawingInfo.pixelMapX,
+        g_drawingInfo.displayY + shadowInfo.y - g_drawingInfo.pixelMapY);
     DrawShadow();
-    int32_t adjustSize = EIGHT_SIZE * GetScaling();
-    g_drawingInfo.rootNodeWidth = g_drawingInfo.pixelMap->GetWidth() + SVG_WIDTH * GetScaling() + adjustSize;
-    g_drawingInfo.rootNodeHeight = g_drawingInfo.pixelMap->GetHeight() + SVG_HEIGHT * GetScaling() + adjustSize;
+    int32_t adjustSize = (SVG_WIDTH + EIGHT_SIZE) * GetScaling();
+    g_drawingInfo.rootNodeWidth = g_drawingInfo.pixelMap->GetWidth() + adjustSize;
+    g_drawingInfo.rootNodeHeight = g_drawingInfo.pixelMap->GetHeight() + adjustSize;
     CHKPR(g_drawingInfo.rootNode, RET_ERR);
     g_drawingInfo.rootNode->SetBounds(0, 0, g_drawingInfo.rootNodeWidth, g_drawingInfo.rootNodeHeight);
     g_drawingInfo.rootNode->SetFrame(0, 0, g_drawingInfo.rootNodeWidth, g_drawingInfo.rootNodeHeight);
     CHKPR(g_drawingInfo.dragWindow, RET_ERR);
     g_drawingInfo.dragWindow->Resize(g_drawingInfo.rootNodeWidth, g_drawingInfo.rootNodeHeight);
-    Draw(g_drawingInfo.displayId, g_drawingInfo.displayX + shadowInfo.x - g_drawingInfo.pixelMapX,
-        g_drawingInfo.displayY + shadowInfo.y - g_drawingInfo.pixelMapY);
     if (g_drawingInfo.sourceType == OHOS::MMI::PointerEvent::SOURCE_TYPE_MOUSE) {
         g_drawingInfo.pixelMapX = shadowInfo.x;
         g_drawingInfo.pixelMapY = shadowInfo.y;
