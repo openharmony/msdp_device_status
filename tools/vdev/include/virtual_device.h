1f37e9eb9d1b9180eebb5bef0057581d0aae722f
kevin812085902 <hugongcheng@huawei.com>
2023-06-29 11:35:27 +0000

    The injection of virtual devices_6
    Signed-off-by: kevin812085902 <hugongcheng@huawei.com>
    Change-Id: I00ea1117179f7aea44c3a4a49252cbee8b1be69f

---
+++ tools/vdev/include/virtual_device.h
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VIRTUAL_DEVICE_H
+#define VIRTUAL_DEVICE_H
+
+#include <map>
+#include <memory>
+#include <vector>
+
+#include <linux/input.h>
+
+#include "nocopyable.h"
+
+#include "v_input_device.h"
+#include "virtual_device_defines.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+class VirtualDevice {
+public:
+    explicit VirtualDevice(const std::string &node);
+    virtual ~VirtualDevice() = default;
+    DISALLOW_COPY_AND_MOVE(VirtualDevice);
+
+    bool IsActive() const;
+    bool IsTouchscreen() const;
+    bool SupportEventType(size_t ev) const;
+    bool SupportKey(size_t key) const;
+    bool SupportAbs(size_t abs) const;
+    bool SupportRel(size_t rel) const;
+    bool SupportMsc(size_t msc) const;
+    bool SupportLed(size_t led) const;
+    bool SupportRep(size_t rep) const;
+    bool SupportProperty(size_t prop) const;
+    bool QueryAbsInfo(size_t abs, struct input_absinfo &absInfo);
+    std::string GetName() const;
+    std::string GetPhys() const;
+    struct input_id GetInputId() const;
+    void SetName(const std::string &name);
+    int32_t SendEvent(uint16_t type, uint16_t code, int32_t value);
+
+protected:
+    static bool FindDeviceNode(const std::string &name, std::string &node);
+    void SetMinimumInterval(int32_t interval);
+
+private:
+    static void Execute(const std::string &command, std::vector<std::string> &results);
+    static void GetInputDeviceNodes(std::map<std::string, std::string> &nodes);
+
+private:
+    int32_t minimumInterval_ { 0 };
+    std::unique_ptr<VInputDevice> inputDev_ { nullptr };
+    std::string name_;
+};
+
+inline bool VirtualDevice::IsActive() const
+{
+    return ((inputDev_ != nullptr) && inputDev_->IsActive());
+}
+
+inline bool VirtualDevice::IsTouchscreen() const
+{
+    return ((inputDev_ != nullptr) && inputDev_->IsTouchscreen());
+}
+
+inline bool VirtualDevice::SupportEventType(size_t ev) const
+{
+    return ((inputDev_ != nullptr) && inputDev_->SupportEventType(ev));
+}
+
+inline bool VirtualDevice::SupportKey(size_t key) const
+{
+    return ((inputDev_ != nullptr) && inputDev_->SupportKey(key));
+}
+
+inline bool VirtualDevice::SupportAbs(size_t abs) const
+{
+    return ((inputDev_ != nullptr) && inputDev_->SupportAbs(abs));
+}
+
+inline bool VirtualDevice::SupportRel(size_t rel) const
+{
+    return ((inputDev_ != nullptr) && inputDev_->SupportRel(rel));
+}
+
+inline bool VirtualDevice::SupportProperty(size_t prop) const
+{
+    return ((inputDev_ != nullptr) && inputDev_->SupportProperty(prop));
+}
+
+inline bool VirtualDevice::SupportMsc(size_t msc) const
+{
+    return ((inputDev_ != nullptr) && inputDev_->SupportMsc(msc));
+}
+
+inline bool VirtualDevice::SupportLed(size_t led) const
+{
+    return ((inputDev_ != nullptr) && inputDev_->SupportLed(led));
+}
+
+inline bool VirtualDevice::SupportRep(size_t rep) const
+{
+    return ((inputDev_ != nullptr) && inputDev_->SupportRep(rep));
+}
+
+inline bool VirtualDevice::QueryAbsInfo(size_t abs, struct input_absinfo &absInfo)
+{
+    return ((inputDev_ != nullptr) && inputDev_->QueryAbsInfo(abs, absInfo));
+}
+
+inline std::string VirtualDevice::GetPhys() const
+{
+    if (inputDev_ != nullptr) {
+        return inputDev_->GetPhys();
+    }
+    return {};
+}
+
+inline struct input_id VirtualDevice::GetInputId() const
+{
+    if (inputDev_ != nullptr) {
+        return inputDev_->GetInputId();
+    }
+    return {};
+}
+
+inline void VirtualDevice::SetName(const std::string &name)
+{
+    name_ = name;
+}
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
+#endif // VIRTUAL_DEVICE_H
---
+++ tools/vdev/src/virtual_device.cpp
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "virtual_device.h"
+
+#include <iostream>
+#include <map>
+
+#include <unistd.h>
+
+#include "devicestatus_define.h"
+#include "fi_log.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+namespace {
+constexpr ::OHOS::HiviewDFX::HiLogLabel LABEL { LOG_CORE, MSDP_DOMAIN_ID, "VirtualDevice" };
+constexpr size_t DEFAULT_BUF_SIZE = 1024;
+constexpr int32_t MINIMUM_INTERVAL_ALLOWED = 1;
+constexpr int32_t MAXIMUM_INTERVAL_ALLOWED = 100;
+} // namespace
+
+VirtualDevice::VirtualDevice(const std::string &node)
+{
+    inputDev_ = std::make_unique<VInputDevice>(node);
+    inputDev_->Open();
+}
+
+bool VirtualDevice::FindDeviceNode(const std::string &name, std::string &node)
+{
+    CALL_DEBUG_ENTER;
+    std::map<std::string, std::string> nodes;
+    GetInputDeviceNodes(nodes);
+    FI_HILOGD("There are %{public}zu device nodes.", nodes.size());
+
+    std::map<std::string, std::string>::const_iterator cItr = nodes.find(name);
+    if (cItr == nodes.cend()) {
+        FI_HILOGE("No virtual stylus is found.");
+        return false;
+    }
+    FI_HILOGD("Node name : \'%{public}s\'.", cItr->second.c_str());
+    std::ostringstream ss;
+    ss << "/dev/input/" << cItr->second;
+    node = ss.str();
+    return true;
+}
+
+void VirtualDevice::Execute(const std::string &command, std::vector<std::string> &results)
+{
+    CALL_DEBUG_ENTER;
+    FI_HILOGD("Execute command:%{public}s.", command.c_str());
+    char buffer[DEFAULT_BUF_SIZE] {};
+    FILE *pin = popen(command.c_str(), "r");
+    if (pin == nullptr) {
+        FI_HILOGE("Failed to popen command.");
+        return;
+    }
+    while (!feof(pin)) {
+        if (fgets(buffer, sizeof(buffer), pin) != nullptr) {
+            results.push_back(buffer);
+        }
+    }
+    FI_HILOGD("Close phandle.");
+    pclose(pin);
+}
+
+void VirtualDevice::GetInputDeviceNodes(std::map<std::string, std::string> &nodes)
+{
+    CALL_DEBUG_ENTER;
+    std::string command = "cat /proc/bus/input/devices";
+    std::vector<std::string> results;
+    Execute(command, results);
+    if (results.empty()) {
+        FI_HILOGE("Failed to list devices.");
+        return;
+    }
+    const std::string kname { "Name=\"" };
+    const std::string kevent { "event" };
+    std::string name;
+    for (const auto &item : results) {
+        FI_HILOGD("item:%{public}s.", item.c_str());
+        if (item[0] == 'N') {
+            std::string::size_type spos = item.find(kname);
+            if (spos != std::string::npos) {
+                spos += kname.size();
+                std::string::size_type tpos = item.find("\"", spos);
+                if (tpos != std::string::npos) {
+                    name = item.substr(spos, tpos - spos);
+                }
+            }
+        } else if (!name.empty() && (item[0] == 'H')) {
+            std::string::size_type spos = item.find(kevent);
+            if (spos != std::string::npos) {
+                std::map<std::string, std::string>::const_iterator cItr = nodes.find(name);
+                if (cItr != nodes.end()) {
+                    nodes.erase(cItr);
+                }
+                std::string::size_type tpos = spos + kevent.size();
+                while (std::isalnum(item[tpos])) {
+                    ++tpos;
+                }
+                nodes.emplace(name, item.substr(spos, tpos - spos));
+                name.clear();
+            }
+        }
+    }
+}
+
+int32_t VirtualDevice::SendEvent(uint16_t type, uint16_t code, int32_t value)
+{
+    CALL_DEBUG_ENTER;
+    CHKPR(inputDev_, RET_ERR);
+    inputDev_->SendEvent(type, code, value);
+
+    if ((type == EV_SYN) && (code == SYN_REPORT)) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(minimumInterval_));
+    }
+    return RET_OK;
+}
+
+std::string VirtualDevice::GetName() const
+{
+    if (!name_.empty()) {
+        return name_;
+    }
+    if (inputDev_ != nullptr) {
+        return inputDev_->GetName();
+    }
+    return {};
+}
+
+void VirtualDevice::SetMinimumInterval(int32_t interval)
+{
+    minimumInterval_ = std::max(MINIMUM_INTERVAL_ALLOWED, std::min(interval, MAXIMUM_INTERVAL_ALLOWED));
+}
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
