bd0ba539685ac58bebf4a547ae5631a559fde1d0
mayunteng_1 <mayunteng@huawei.com>
2023-07-15 07:15:09 +0000

    解决穿越及拖拽问题
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: If5cfd965d4420f5e5db388c7a8c3cc13c87dec23

--- services/interaction/coordination/src/coordination_sm.cpp
+++ services/interaction/coordination/src/coordination_sm.cpp
@@ -45,6 +45,7 @@ constexpr float MOUSE_ABS_LOCATION { 100 };
 constexpr int32_t MOUSE_ABS_LOCATION_X { 50 };
 constexpr int32_t MOUSE_ABS_LOCATION_Y { 50 };
 constexpr int32_t COORDINATION_PRIORITY { 499 };
+constexpr int32_t SOFTBUS_TIME { 8000 };
 constexpr int32_t MIN_HANDLER_ID { 1 };
 } // namespace
 
@@ -66,6 +67,10 @@ void CoordinationSM::Init()
     CHKPV(context);
     context->GetTimerManager().AddTimer(INTERVAL_MS, 1, [this]() {
         this->InitDeviceManager();
+        COOR_SOFTBUS_ADAPTER->Init();
+    });
+    context->GetTimerManager().AddTimer(INTERVAL_MS, 1, [this]() {
+        FI_HILOGE("COOR_SOFTBUS_ADAPTER start");
         COOR_SOFTBUS_ADAPTER->Init();
     });
     COOR_DEV_MGR->Init();
