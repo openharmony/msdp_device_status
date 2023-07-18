15f148cba1cd5b0e0746bfc1735c685d7c523e9a
dongzhanwu <dongzhanwu1@huawei.com>
2023-07-18 16:15:39 +0800

    Added real-time coordinate display function of touch screen action
    Signed-off-by: dongzhanwu <dongzhanwu1@huawei.com>

--- tools/vdev/src/virtual_touchscreen_builder.cpp
+++ tools/vdev/src/virtual_touchscreen_builder.cpp
@@ -44,6 +44,7 @@ constexpr int32_t ABS_MT_ORIENTATION_MAX { 90 };
 constexpr int32_t ABS_MT_BLOB_ID_MAX { 10 };
 constexpr int32_t ABS_MT_TRACKING_ID_MAX { 9 };
 constexpr int32_t ABS_TOOL_TYPE_MAX { 15 };
+constexpr uint32_t IO_FLAG_WIDTH{ 6 };
 constexpr uint32_t SY_OFFSET { 1 };
 constexpr uint32_t TX_OFFSET { 2 };
 constexpr uint32_t TY_OFFSET { 3 };
@@ -69,7 +70,7 @@ void PointerEventMonitor::OnInputEvent(std::shared_ptr<MMI::PointerEvent> pointe
     if (!pointerEvent->GetPointerItem(pointerEvent->GetPointerId(), pointerItem)) {
         return;
     }
-    std::cout << "\rcurrent touch position - x: " << std::setw(6) << std::left
+    std::cout << "\rcurrent touch position - x: " << std::setw(IO_FLAG_WIDTH) << std::left
         << pointerItem.GetDisplayX() << "y: " << pointerItem.GetDisplayY() << "            ";
     std::cout.flush();
 }
