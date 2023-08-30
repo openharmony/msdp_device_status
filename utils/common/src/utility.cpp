da0a11ecee1275269e4f97d91deef074bb42eb11
mrsongliang <songliangliang5@huawei.com>
2023-08-30 03:44:20 +0000

    Signed-off-by:mrsongliang<songliangliang5@huawei.com>
    Signed-off-by: mrsongliang <songliangliang5@huawei.com>
    Change-Id: I82d271d9fffa88c6fe6224815952fa2f38fc5f22

--- utils/common/src/utility.cpp
+++ utils/common/src/utility.cpp
@@ -147,8 +147,8 @@ void Utility::ShowFileAttributes(const char *path)
     }
 
     std::ostringstream ss;
-    std::map<mode_t, std::string> modes {{S_IRUSR, "U+R"}, {S_IWUSR, "U+W"}, {S_IXUSR, "U+X"}, {S_IRGRP, "G+R"}, {S_IWGRP, "G+W"},
-        {S_IXGRP, "G+X"}, {S_IROTH, "O+R"}, {S_IWOTH, "O+W"}, {S_IXOTH, "O+X"}};
+    std::map<mode_t, std::string> modes {{S_IRUSR, "U+R "}, {S_IWUSR, "U+W "}, {S_IXUSR, "U+X "}, {S_IRGRP, "G+R "}, {S_IWGRP, "G+W "},
+        {S_IXGRP, "G+X "}, {S_IROTH, "O+R "}, {S_IWOTH, "O+W "}, {S_IXOTH, "O+X "}};
     for (auto element : modes) {
         if (buf.st_mode & element.first) {
             ss << element.second;
