f08569a9ddac862eb4704cb835abc0fc1e862622
hewanglon <hewanglong@huawei.com>
2023-11-02 01:49:31 +0000

    add getDragDataStateFuzz
    Signed-off-by: hewanglon <hewanglong@huawei.com>
    Change-Id: I8d585ffc2f7aeb5d2f377b7b8527cf9d648e18ce

---
+++ test/fuzztest/getdragstate_fuzzer/getdragstate_fuzzer.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "getdragstate_fuzzer.h"
+
+#include "singleton.h"
+
+#include "devicestatus_service.h"
+#include "fi_log.h"
+#include "message_parcel.h"
+
+using namespace OHOS::Msdp::DeviceStatus;
+
+namespace OHOS {
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL { LOG_CORE, Msdp::MSDP_DOMAIN_ID, "GetDragStateFuzzTest" };
+
+bool GetDragStateFuzzTest(const uint8_t* data, size_t size)
+{
+    const std::u16string FORMMGR_DEVICE_TOKEN { u"ohos.msdp.Idevicestatus" };
+    MessageParcel datas;
+    if (!datas.WriteInterfaceToken(FORMMGR_DEVICE_TOKEN) || !datas.WriteBuffer(data, size) || !datas.RewindRead(0)) {
+        FI_HILOGE("Write failed");
+        return false;
+    }
+    MessageParcel reply;
+    MessageOption option;
+    DelayedSingleton<DeviceStatusService>::GetInstance()->OnRemoteRequest(
+        static_cast<uint32_t>(Msdp::DeviceInterfaceCode::GET_DRAG_DATA), datas, reply, option);
+    return true;
+}
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+    
+    OHOS::GetDragStateFuzzTest(data, size);
+    return 0;
+}
