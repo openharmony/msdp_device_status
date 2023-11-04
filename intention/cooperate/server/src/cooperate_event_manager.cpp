d2bd21b620da573d491fb5b38afe1deefbe71711
baoshubei <baoshubei@huawei.com>
2023-11-04 07:58:00 +0000

    fix conflict
    Signed-off-by: baoshubei <baoshubei@huawei.com>
    Change-Id: I87e6735755e488681cf02b9d0a2dfa98900118eb

--- intention/cooperate/server/src/cooperate_event_manager.cpp
+++ intention/cooperate/server/src/cooperate_event_manager.cpp
@@ -84,7 +84,7 @@ void CooperateEventManager::OnEnable(CooperateMessage msg, const std::string &ne
     sptr<EventInfo> info = cooperateCallbacks_[EventType::ENABLE];
     CHKPV(info);
     NotifyCooperateMessage(info->sess, info->msgId, info->userData, networkId, msg);
-    cooperateCallbacks_[EventType::ENABLE] = nullptr;
+    cooperateCallbacks_[EventType::ENABLE] =  nullptr;
 }
 
 void CooperateEventManager::OnStart(CooperateMessage msg, const std::string &networkId)
@@ -94,7 +94,7 @@ void CooperateEventManager::OnStart(CooperateMessage msg, const std::string &net
     sptr<EventInfo> info = cooperateCallbacks_[EventType::START];
     CHKPV(info);
     NotifyCooperateMessage(info->sess, info->msgId, info->userData, networkId, msg);
-    cooperateCallbacks_[EventType::START] = nullptr;
+    cooperateCallbacks_[EventType::START] =  nullptr;
 }
 
 void CooperateEventManager::OnStop(CooperateMessage msg, const std::string &networkId)
@@ -104,7 +104,7 @@ void CooperateEventManager::OnStop(CooperateMessage msg, const std::string &netw
     sptr<EventInfo> info = cooperateCallbacks_[EventType::STOP];
     CHKPV(info);
     NotifyCooperateMessage(info->sess, info->msgId, info->userData, networkId, msg);
-    cooperateCallbacks_[EventType::STOP] = nullptr;
+    cooperateCallbacks_[EventType::STOP] =  nullptr;
 }
 
 void CooperateEventManager::OnGetCrossingSwitchState(bool state)
