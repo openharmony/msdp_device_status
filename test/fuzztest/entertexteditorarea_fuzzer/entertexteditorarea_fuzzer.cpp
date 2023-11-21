4cdef7bb83231494277f33f889d0a624286f1fc9
yangjintao <yangjintao16@huawei.com>
2023-11-21 08:45:44 +0000

    add EnterTextEditorArea
    Change-Id: Id54acf99c50b55ce1ab3828488b973c3379b2889
    Signed-off-by: yangjintao <yangjintao16@huawei.com>

---
+++ test/fuzztest/entertexteditorarea_fuzzer/entertexteditorarea_fuzzer.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "entertexteditorarea_fuzzer.h"
+
+#include "singleton.h"
+
+#define private public
+#include "devicestatus_service.h"
+#include "message_parcel.h"
+
+using namespace OHOS::Msdp::DeviceStatus;
+namespace OHOS {
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL { LOG_CORE, OHOS::Msdp::MSDP_DOMAIN_ID, "EnterTextEditorAreaFuzzTest" };
+
+bool EnterTextEditorAreaFuzzTest(const uint8_t* data, size_t size)
+{
+    const std::u16string FORMMGR_DEVICE_TOKEN { u"ohos.msdp.Idevicestatus" };
+    MessageParcel datas;
+    if (!datas.WriteInterfaceToken(FORMMGR_DEVICE_TOKEN)) {
+        FI_HILOGE("Write failed");
+        return false;
+    }
+    if (!datas.WriteBuffer(data, size)) {
+        FI_HILOGE("Write data failed");
+        return false;
+    }
+    if (!datas.RewindRead(0)) {
+        FI_HILOGE("Read failed");
+        return false;
+    }
+    MessageParcel reply;
+    MessageOption option;
+    DelayedSingleton<DeviceStatusService>::GetInstance()->OnRemoteRequest(
+        static_cast<uint32_t>(Msdp::DeviceInterfaceCode::ENTER_TEXT_EDITOR_AREA), datas, reply, option);
+    return true;
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+
+    OHOS::EnterTextEditorAreaFuzzTest(data, size);
+    return 0;
+}
+
