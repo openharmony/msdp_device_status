032ed8237e8055aeafbcd2ca17766769bb91f73f
wangruitao <wangruitao10@huawei.com>
2023-11-28 08:57:06 +0000

    state machine
    Signed-off-by: wangruitao <wangruitao10@huawei.com>

---
+++ intention/cooperate/state_machine/include/cooperate_in.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef COOPERATE_IN_H
+#define COOPERATE_IN_H
+
+#include <array>
+#include <memory>
+
+#include "i_cooperate_state.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+class CooperateIn : public ICooperateState {
+public:
+    CooperateIn();
+    ~CooperateIn() = default;
+
+    void OnEvent(Context &context, CooperateEvent &event) override;
+    void OnEnterState(Context &event) override;
+    void OnLeaveState(Context &event) override;
+
+private:
+    class Initial final : public ICooperateStep {
+    public:
+        void OnEvent(Context &context, CooperateEvent &event) override;
+        void OnProgress(Context &context, CooperateEvent &event) override;
+        void OnReset(Context &context, CooperateEvent &event) override;
+    };
+
+    class PrepareRemoteInput final : public ICooperateStep {
+    public:
+        void OnEvent(Context &context, CooperateEvent &event) override;
+        void OnProgress(Context &context, CooperateEvent &event) override;
+        void OnReset(Context &context, CooperateEvent &event) override;
+    
+    private:
+        std::pair<std::string, std::string> prepared_;
+    };
+
+    class StartRemoteInput final : public ICooperateStep {
+    public:
+        void OnEvent(Context &context, CooperateEvent &event) override;
+        void OnProgress(Context &context, CooperateEvent &event) override;
+        void OnReset(Context &context, CooperateEvent &event) override;
+    };
+};
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
+#endif // COOPERATE_IN_H
---
+++ intention/cooperate/state_machine/src/i_cooperate_state.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "i_cooperate_state.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+
+ICooperateState::ICooperateStep::ICooperateStep(ICooperateState &parent, std::shared_ptr<ICooperateStep> prev)
+    : parent_(parent), prev_(prev)
+{}
+
+void ICooperateState::Switch(std::shared_ptr<ICooperateStep> step)
+{
+    if (step != nullptr) {
+        current_ = step;
+    }
+}
+
+void ICooperateState::ICooperateStep::SetNext(std::shared_ptr<ICooperateStep> next)
+{
+    next_ = next;
+}
+
+void ICooperateState::ICooperateStep::Switch(std::shared_ptr<ICooperateStep> step)
+{
+    parent_.Switch(step);
+}
+void ICooperateState::ICooperateStep::Proceed(Context &context, CooperateEvent &event)
+{
+    if (next_ != nullptr) {
+        Switch(next_);
+        next_->OnProgress(context, event);
+    }
+}
+
+void ICooperateState::ICooperateStep::Reset(Context &context, CooperateEvent &event)
+{
+    if (prev_ != nullptr) {
+        Switch(prev_);
+        prev_->OnReset(context, event);
+    }
+}
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
