30a3b96c7927450ff991c16e8b1d7eb97ec9bd33
mayunteng_1 <mayunteng@huawei.com>
2023-12-20 09:50:35 +0000

    会话打开失败则关闭
    Signed-off-by: mayunteng_1 <mayunteng@huawei.com>
    Change-Id: Ib6518cffa6cdc6ceab1b789c8781e04a8c4ba41d

--- services/interaction/coordination/src/coordination_softbus_adapter.cpp
+++ services/interaction/coordination/src/coordination_softbus_adapter.cpp
@@ -191,7 +191,7 @@ int32_t CoordinationSoftbusAdapter::Init()
         FI_HILOGI("Server set ok");
     } else {
         FI_HILOGE("Server set failed, ret:%{public}d", ret);
-        ShutDown(socketFd_);
+        Shutdown(socketFd_);
     }
     return ret;
 }
@@ -275,7 +275,7 @@ int32_t CoordinationSoftbusAdapter::OpenInputSoftbus(const std::string &remoteNe
         ConfigTcpAlive(socket);
     } else {
         FI_HILOGE("Bind failed, ret:%{public}d", ret);
-        ShutDown(socket);
+        Shutdown(socket);
     }
     return ret;
 }
