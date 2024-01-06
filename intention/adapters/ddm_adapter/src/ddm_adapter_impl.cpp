b94a5252b84b4be3898dd86d0e014c387351e5b5
wangruitao <wangruitao10@huawei.com>
2024-01-06 07:32:09 +0000

    文件提交
    Signed-off-by: wangruitao <wangruitao10@huawei.com>

---
+++ intention/adapters/ddm_adapter/src/ddm_adapter_impl.cpp
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ddm_adapter_impl.h"
+
+#include <algorithm>
+
+#include "devicestatus_define.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+namespace {
+constexpr HiviewDFX::HiLogLabel LABEL { LOG_CORE, MSDP_DOMAIN_ID, "DDMAdapterImpl" };
+}
+
+#define D_DEV_MGR   DistributedHardware::DeviceManager::GetInstance()
+
+DDMAdapterImpl::~DDMAdapterImpl()
+{
+    Disable();
+}
+
+int32_t DDMAdapterImpl::Enable()
+{
+    std::lock_guard guard(lock_);
+    std::string pkgName(FI_PKG_NAME);
+    std::string extra;
+    initCb_ = std::make_shared<DmInitCb>();
+
+    int32_t ret = D_DEV_MGR.InitDeviceManager(pkgName, initCb_);
+    if (ret != 0) {
+        FI_HILOGE("DM::InitDeviceManager fail");
+        goto INIT_FAIL;
+    }
+    boardStateCb_ = std::make_shared<DmBoardStateCb>(shared_from_this());
+
+    ret = D_DEV_MGR.RegisterDevStateCallback(pkgName, extra, boardStateCb_);
+    if (ret != 0) {
+        FI_HILOGE("DM::RegisterDevStateCallback fail");
+        goto REG_FAIL;
+    }
+    return RET_OK;
+
+REG_FAIL:
+    ret = D_DEV_MGR.UnInitDeviceManager(pkgName);
+    if (ret != 0) {
+        FI_HILOGE("DM::UnInitDeviceManager fail");
+    }
+    boardStateCb_.reset();
+
+INIT_FAIL:
+    initCb_.reset();
+    return RET_ERR;
+}
+
+void DDMAdapterImpl::Disable()
+{
+    std::lock_guard guard(lock_);
+    std::string pkgName(FI_PKG_NAME);
+
+    if (boardStateCb_ != nullptr) {
+        boardStateCb_.reset();
+        int32_t ret = D_DEV_MGR.UnRegisterDevStateCallback(pkgName);
+        if (ret != 0) {
+            FI_HILOGE("DM::UnRegisterDevStateCallback fail");
+        }
+    }
+    if (initCb_ != nullptr) {
+        initCb_.reset();
+        int32_t ret = D_DEV_MGR.UnInitDeviceManager(pkgName);
+        if (ret != 0) {
+            FI_HILOGE("DM::UnInitDeviceManager fail");
+        }
+    }
+}
+
+void DDMAdapterImpl::AddBoardObserver(std::shared_ptr<IBoardObserver> observer)
+{
+    std::lock_guard guard(lock_);
+    CHKPV(observer);
+    observers_.erase(Observer());
+    observers_.emplace(observer);
+}
+
+void DDMAdapterImpl::RemoveBoardObserver(std::shared_ptr<IBoardObserver> observer)
+{
+    std::lock_guard guard(lock_);
+    CHKPV(observer);
+    observers_.erase(Observer());
+    if (auto iter = observers_.find(Observer(observer)); iter != observers_.end()) {
+        observers_.erase(iter);
+    }
+}
+
+void DDMAdapterImpl::OnBoardOnline(const std::string &networkId)
+{
+    std::lock_guard guard(lock_);
+    std::for_each(observers_.cbegin(), observers_.cend(),
+        [&networkId](const auto &item) {
+            if (auto observer = item.Lock(); observer != nullptr) {
+                observer->OnBoardOnline(networkId);
+            }
+        });
+}
+
+void DDMAdapterImpl::OnBoardOffline(const std::string &networkId)
+{
+    std::lock_guard guard(lock_);
+    std::for_each(observers_.cbegin(), observers_.cend(),
+        [&networkId](const auto &item) {
+            if (auto observer = item.Lock(); observer != nullptr) {
+                observer->OnBoardOffline(networkId);
+            }
+        });
+}
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
