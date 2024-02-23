be41dc2ae329cd3bc63a06964d4ed6117fc0e603
blueyouh <bailiang18@huawei.com>
2024-02-23 11:44:36 +0000

    build cooperate (part 2)
    Signed-off-by: blueyouh <bailiang18@huawei.com>
    Change-Id: I87048ed50a7d5c417bdaf7e4a9efed2db16ba774

---
+++ intention/cooperate/plugin/src/input_event_transmission/input_event_builder.cpp
@@ -0,0 +1,213 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "input_event_transmission/input_event_builder.h"
+
+#include "display_info.h"
+
+#include "cooperate_context.h"
+#include "devicestatus_define.h"
+#include "input_event_transmission/input_event_serialization.h"
+#include "utility.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+namespace Cooperate {
+namespace {
+constexpr HiviewDFX::HiLogLabel LABEL { LOG_CORE, MSDP_DOMAIN_ID, "InputEventBuilder" };
+} // namespace
+
+InputEventBuilder::InputEventBuilder(IContext *env)
+    : env_(env)
+{
+    observer_ = std::make_shared<DSoftbusObserver>(*this);
+    pointerEvent_ = MMI::PointerEvent::Create();
+    keyEvent_ = MMI::KeyEvent::Create();
+}
+
+void InputEventBuilder::Enable(Context &context)
+{
+    CALL_INFO_TRACE;
+    cursorPos_.pos = context.CursorPosition();
+    remoteNetworkId_ = context.Peer();
+    env_->GetDSoftbus().AddObserver(observer_);
+    FI_HILOGI("Cursor transite in (%{public}d, %{public}d)", cursorPos_.pos.x, cursorPos_.pos.y);
+}
+
+void InputEventBuilder::Disable()
+{
+    CALL_INFO_TRACE;
+    env_->GetDSoftbus().RemoveObserver(observer_);
+    remoteNetworkId_.clear();
+}
+
+void InputEventBuilder::Update(Context &context)
+{
+    CALL_INFO_TRACE;
+    remoteNetworkId_ = context.Peer();
+}
+
+bool InputEventBuilder::OnPacket(const std::string &networkId, Msdp::NetPacket &packet)
+{
+    CALL_DEBUG_ENTER;
+    if (networkId != remoteNetworkId_) {
+        return false;
+    }
+    switch (packet.GetMsgId()) {
+        case MessageId::DSOFTBUS_INPUT_POINTER_EVENT: {
+            OnPointerEvent(packet);
+            break;
+        }
+        case MessageId::DSOFTBUS_INPUT_KEY_EVENT: {
+            OnKeyEvent(packet);
+            break;
+        }
+        default: {
+            return false;
+        }
+    }
+    return true;
+}
+
+void InputEventBuilder::OnPointerEvent(Msdp::NetPacket &packet)
+{
+    CALL_DEBUG_ENTER;
+    CHKPV(pointerEvent_);
+    int32_t ret = InputEventSerialization::Unmarshalling(packet, pointerEvent_);
+    if (ret != RET_OK) {
+        FI_HILOGE("Failed to deserialize pointer event");
+        return;
+    }
+    if (!AccumulateMotion(pointerEvent_) ||
+        !UpdatePointerEvent(pointerEvent_)) {
+        return;
+    }
+    env_->GetInput().SimulateInputEvent(pointerEvent_);
+    pointerEvent_->Reset();
+}
+
+void InputEventBuilder::OnKeyEvent(Msdp::NetPacket &packet)
+{
+    CALL_DEBUG_ENTER;
+    CHKPV(keyEvent_);
+    int32_t ret = InputEventSerialization::NetPacketToKeyEvent(packet, keyEvent_);
+    if (ret != RET_OK) {
+        FI_HILOGE("Failed to deserialize key event");
+        return;
+    }
+    env_->GetInput().SimulateInputEvent(keyEvent_);
+    ResetKeyEvent(keyEvent_);
+}
+
+bool InputEventBuilder::AccumulateMotion(std::shared_ptr<MMI::PointerEvent> pointerEvent)
+{
+    if ((pointerEvent->GetSourceType() != MMI::PointerEvent::SOURCE_TYPE_MOUSE) ||
+        (pointerEvent->GetPointerAction() != MMI::PointerEvent::POINTER_ACTION_MOVE)) {
+        return true;
+    }
+    MMI::PointerEvent::PointerItem item;
+
+    if (!pointerEvent->GetPointerItem(pointerEvent->GetPointerId(), item)) {
+        FI_HILOGE("Corrupted pointer event");
+        return false;
+    }
+    cursorPos_.pos.x += item.GetDisplayX();
+    cursorPos_.pos.y += item.GetDisplayY();
+
+    if (cursorPos_.displayId < 0) {
+        auto display = Rosen::DisplayManager::GetInstance().GetDefaultDisplay();
+        CHKPF(display);
+        cursorPos_.displayId = display->GetId();
+    }
+    auto targetDisplay = Rosen::DisplayManager::GetInstance().GetDisplayById(cursorPos_.displayId);
+    if (targetDisplay == nullptr) {
+        targetDisplay = Rosen::DisplayManager::GetInstance().GetDefaultDisplay();
+        CHKPF(targetDisplay);
+    }
+    if (IsInDisplay(targetDisplay)) {
+        return true;
+    }
+    std::vector<sptr<Rosen::Display>> displays = Rosen::DisplayManager::GetInstance().GetAllDisplays();
+
+    for (const auto &display : displays) {
+        if ((display != nullptr) && IsInDisplay(display)) {
+            targetDisplay = display;
+            break;
+        }
+    }
+    sptr<Rosen::DisplayInfo> targetDisplayInfo = targetDisplay->GetDisplayInfo();
+    CHKPF(targetDisplayInfo);
+    cursorPos_.displayId = targetDisplay->GetId();
+    cursorPos_.pos.x = std::clamp<int32_t>(cursorPos_.pos.x, targetDisplayInfo->GetOffsetX(),
+        targetDisplayInfo->GetOffsetX() + targetDisplayInfo->GetWidth() - 1);
+    cursorPos_.pos.y = std::clamp<int32_t>(cursorPos_.pos.y, targetDisplayInfo->GetOffsetY(),
+        targetDisplayInfo->GetOffsetY() + targetDisplayInfo->GetHeight() - 1);
+    FI_HILOGD("Cursor move to [%{public}d](%{public}d, %{public}d)",
+        pointerEvent->GetId(), cursorPos_.pos.x, cursorPos_.pos.y);
+    return true;
+}
+
+bool InputEventBuilder::IsInDisplay(sptr<Rosen::Display> display) const
+{
+    sptr<Rosen::DisplayInfo> displayInfo = display->GetDisplayInfo();
+    CHKPF(displayInfo);
+    return ((cursorPos_.pos.x >= displayInfo->GetOffsetX()) &&
+            (cursorPos_.pos.x < displayInfo->GetOffsetX() + displayInfo->GetWidth()) &&
+            (cursorPos_.pos.y >= displayInfo->GetOffsetY()) &&
+            (cursorPos_.pos.y < displayInfo->GetOffsetY() + displayInfo->GetHeight()));
+}
+
+bool InputEventBuilder::UpdatePointerEvent(std::shared_ptr<MMI::PointerEvent> pointerEvent)
+{
+    if (pointerEvent->GetSourceType() != MMI::PointerEvent::SOURCE_TYPE_MOUSE) {
+        return true;
+    }
+    MMI::PointerEvent::PointerItem item;
+    if (!pointerEvent->GetPointerItem(pointerEvent->GetPointerId(), item)) {
+        FI_HILOGE("Corrupted pointer event");
+        return false;
+    }
+    item.SetDisplayX(cursorPos_.pos.x);
+    item.SetDisplayY(cursorPos_.pos.y);
+    pointerEvent->UpdatePointerItem(pointerEvent->GetPointerId(), item);
+
+    int64_t time = Utility::GetSysClockTime();
+    pointerEvent->SetActionTime(time);
+    pointerEvent->SetActionStartTime(time);
+    pointerEvent->SetTargetDisplayId(cursorPos_.displayId);
+    pointerEvent->SetTargetWindowId(-1);
+    pointerEvent->SetAgentWindowId(-1);
+    // Tag remote event.
+    pointerEvent->SetDeviceId(
+        (pointerEvent->GetDeviceId() >= 0) ?
+        -(pointerEvent->GetDeviceId() + 1) :
+        pointerEvent->GetDeviceId());
+    return true;
+}
+
+void InputEventBuilder::ResetKeyEvent(std::shared_ptr<MMI::KeyEvent> keyEvent) const
+{
+    std::vector<MMI::KeyEvent::KeyItem> keys = keyEvent->GetKeyItems();
+
+    for (MMI::KeyEvent::KeyItem &item : keys) {
+        item.SetPressed(false);
+        keyEvent->RemoveReleasedKeyItems(item);
+    }
+}
+} // namespace Cooperate
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
---
+++ intention/cooperate/plugin/src/input_event_transmission/input_event_interceptor.cpp
@@ -0,0 +1,229 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "input_event_transmission/input_event_interceptor.h"
+
+#include "cooperate_context.h"
+#include "devicestatus_define.h"
+#include "display_manager.h"
+#include "input_event_transmission/input_event_serialization.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+namespace Cooperate {
+namespace {
+constexpr HiviewDFX::HiLogLabel LABEL { LOG_CORE, MSDP_DOMAIN_ID, "InputEventInterceptor" };
+constexpr float HALF_RATIO { 0.5 };
+} // namespace
+
+std::set<int32_t> InputEventInterceptor::filterKeys_ {
+    MMI::KeyEvent::KEYCODE_BACK,
+    MMI::KeyEvent::KEYCODE_VOLUME_UP,
+    MMI::KeyEvent::KEYCODE_VOLUME_DOWN,
+    MMI::KeyEvent::KEYCODE_POWER,
+};
+
+void InputEventInterceptor::Enable(Context &context)
+{
+    CALL_INFO_TRACE;
+    cursorPos_ = context.CursorPosition();
+    remoteNetworkId_ = context.Peer();
+    startDeviceId_ = context.StartDeviceId();
+    sender_ = context.Sender();
+    FI_HILOGI("Cursor transite out at (%{public}d, %{public}d)", cursorPos_.x, cursorPos_.y);
+    SetupBound();
+    CheckBound();
+
+    interceptorId_ = env_->GetInput().AddInterceptor(
+        std::bind(&InputEventInterceptor::OnPointerEvent, this, std::placeholders::_1),
+        std::bind(&InputEventInterceptor::OnKeyEvent, this, std::placeholders::_1));
+    if (interceptorId_ < 0) {
+        FI_HILOGE("Input::AddInterceptor fail");
+    }
+}
+
+void InputEventInterceptor::Disable()
+{
+    CALL_INFO_TRACE;
+    remoteNetworkId_.clear();
+    startDeviceId_ = -1;
+
+    if (interceptorId_ > 0) {
+        env_->GetInput().RemoveInterceptor(interceptorId_);
+        interceptorId_ = -1;
+    }
+}
+
+void InputEventInterceptor::Update(Context &context)
+{
+    CALL_INFO_TRACE;
+    remoteNetworkId_ = context.Peer();
+}
+
+void InputEventInterceptor::SetupBound()
+{
+    auto display = Rosen::DisplayManager::GetInstance().GetDefaultDisplay();
+    if (display == nullptr) {
+        FI_HILOGE("No default display");
+        displayBound_ = {};
+        cursorBound_ = {};
+        return;
+    }
+    displayBound_ = Rectangle {
+        .width = display->GetWidth(),
+        .height = display->GetHeight(),
+    };
+    if ((displayBound_.width <= 0) || (displayBound_.height <= 0)) {
+        FI_HILOGE("Invalid display information");
+        return;
+    }
+    displayBound_.x = static_cast<int32_t>(displayBound_.width * HALF_RATIO);
+    displayBound_.y = static_cast<int32_t>(displayBound_.height * HALF_RATIO);
+
+    constexpr double boundRatio { 0.8 };
+    cursorBound_.width = static_cast<int32_t>(displayBound_.width * boundRatio);
+    cursorBound_.height = static_cast<int32_t>(displayBound_.height * boundRatio);
+    cursorBound_.x = (displayBound_.x - static_cast<int32_t>(cursorBound_.width * HALF_RATIO));
+    cursorBound_.y = (displayBound_.y - static_cast<int32_t>(cursorBound_.height * HALF_RATIO));
+    FI_HILOGI("Display bound (%{public}d, %{public}d)[%{public}d, %{public}d]",
+        displayBound_.x, displayBound_.y, displayBound_.width, displayBound_.height);
+    FI_HILOGI("Cursor bound (%{public}d, %{public}d)[%{public}d, %{public}d]",
+        cursorBound_.x, cursorBound_.y, cursorBound_.width, cursorBound_.height);
+}
+
+void InputEventInterceptor::CheckBound()
+{
+    if ((cursorBound_.width <= 0) || (cursorBound_.height <= 0)) {
+        return;
+    }
+    if ((cursorPos_.x < cursorBound_.x) ||
+        (cursorPos_.x >= (cursorBound_.x + cursorBound_.width)) ||
+        (cursorPos_.y < cursorBound_.y) ||
+        (cursorPos_.y >= (cursorBound_.y + cursorBound_.height))) {
+        inTrans_ = true;
+        transCursorPos_ = cursorPos_;
+        env_->GetInput().SetPointerVisibility(false);
+        env_->GetInput().SetPointerLocation(displayBound_.x, displayBound_.y);
+        FI_HILOGD("Cursor is out of bound, reset to (%{public}d, %{public}d)", displayBound_.x, displayBound_.y);
+    }
+}
+
+void InputEventInterceptor::CheckTrans(const Coordinate &cursorPos)
+{
+    if (!inTrans_) {
+        return;
+    }
+    double d1 = ::hypot(cursorPos.x - transCursorPos_.x, cursorPos.y - transCursorPos_.y);
+    double d2 = ::hypot(cursorPos.x - displayBound_.x, cursorPos.y - displayBound_.y);
+    if (d2 < d1) {
+        inTrans_ = false;
+        cursorPos_.x = displayBound_.x;
+        cursorPos_.y = displayBound_.y;
+    }
+}
+
+void InputEventInterceptor::OnPointerEvent(std::shared_ptr<MMI::PointerEvent> pointerEvent)
+{
+    CHKPV(pointerEvent);
+    if (pointerEvent->GetDeviceId() != startDeviceId_) {
+        ReportPointerEvent(pointerEvent);
+        return;
+    }
+    if (!DifferentiateMotion(pointerEvent)) {
+        return;
+    }
+    NetPacket packet(MessageId::DSOFTBUS_INPUT_POINTER_EVENT);
+
+    int32_t ret = InputEventSerialization::Marshalling(pointerEvent, packet);
+    if (ret != RET_OK) {
+        FI_HILOGE("Failed to serialize pointer event");
+        return;
+    }
+    env_->GetDSoftbus().SendPacket(remoteNetworkId_, packet);
+}
+
+void InputEventInterceptor::OnKeyEvent(std::shared_ptr<MMI::KeyEvent> keyEvent)
+{
+    CHKPV(keyEvent);
+    if (filterKeys_.find(keyEvent->GetKeyCode()) != filterKeys_.end()) {
+        keyEvent->AddFlag(MMI::AxisEvent::EVENT_FLAG_NO_INTERCEPT);
+        env_->GetInput().SimulateInputEvent(keyEvent);
+        return;
+    }
+    NetPacket packet(MessageId::DSOFTBUS_INPUT_KEY_EVENT);
+
+    int32_t ret = InputEventSerialization::KeyEventToNetPacket(keyEvent, packet);
+    if (ret != RET_OK) {
+        FI_HILOGE("Failed to serialize key event");
+        return;
+    }
+    env_->GetDSoftbus().SendPacket(remoteNetworkId_, packet);
+}
+
+void InputEventInterceptor::ReportPointerEvent(std::shared_ptr<MMI::PointerEvent> pointerEvent)
+{
+    MMI::PointerEvent::PointerItem pointerItem;
+
+    if (!pointerEvent->GetPointerItem(pointerEvent->GetPointerId(), pointerItem)) {
+        FI_HILOGE("Corrupted pointer event");
+        return;
+    }
+    sender_.Send(CooperateEvent(
+        CooperateEventType::INPUT_POINTER_EVENT,
+        InputPointerEvent {
+            .deviceId = pointerEvent->GetDeviceId(),
+            .pointerAction = pointerEvent->GetPointerAction(),
+            .sourceType = pointerEvent->GetSourceType(),
+            .position = Coordinate {
+                .x = pointerItem.GetDisplayX(),
+                .y = pointerItem.GetDisplayY(),
+            }
+        }));
+}
+
+bool InputEventInterceptor::DifferentiateMotion(std::shared_ptr<MMI::PointerEvent> pointerEvent)
+{
+    if ((pointerEvent->GetSourceType() != MMI::PointerEvent::SOURCE_TYPE_MOUSE) ||
+        (pointerEvent->GetPointerAction() != MMI::PointerEvent::POINTER_ACTION_MOVE)) {
+        return true;
+    }
+    MMI::PointerEvent::PointerItem item;
+
+    if (!pointerEvent->GetPointerItem(pointerEvent->GetPointerId(), item)) {
+        FI_HILOGE("Corrupted pointer event");
+        return false;
+    }
+    CheckTrans(Coordinate {
+        .x = item.GetDisplayX(),
+        .y = item.GetDisplayY(),
+    });
+    Coordinate delta {
+        .x = (item.GetDisplayX() - cursorPos_.x),
+        .y = (item.GetDisplayY() - cursorPos_.y),
+    };
+    FI_HILOGD("Cursor moved [%{public}d](%{public}d, %{public}d)", pointerEvent->GetId(), delta.x, delta.y);
+    cursorPos_.x = item.GetDisplayX();
+    cursorPos_.y = item.GetDisplayY();
+    item.SetDisplayX(delta.x);
+    item.SetDisplayY(delta.y);
+    pointerEvent->UpdatePointerItem(pointerEvent->GetPointerId(), item);
+    CheckBound();
+    return true;
+}
+} // namespace Cooperate
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
