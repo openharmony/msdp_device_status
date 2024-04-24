04e1a173c03c509893e779cb0520ece484e45cf9
zzs110 <zhouzishuai@huawei.com>
2024-04-24 17:29:23 +0800

    拖拽通知位置信息到中转站
    Signed-off-by: zzs110 <zhouzishuai@huawei.com>

--- services/interaction/drag/src/drag_drawing.cpp
+++ services/interaction/drag/src/drag_drawing.cpp
@@ -151,11 +151,7 @@ const std::string FORBID_ONE_DRAG_PATH { "/system/etc/device_status/drag_icon/Fo
 const std::string MOUSE_DRAG_DEFAULT_PATH { "/system/etc/device_status/drag_icon/Mouse_Drag_Default.svg" };
 const std::string MOUSE_DRAG_CURSOR_CIRCLE_PATH { "/system/etc/device_status/drag_icon/Mouse_Drag_Cursor_Circle.png" };
 const std::string MOVE_DRAG_PATH { "/system/etc/device_status/drag_icon/Move_Drag.svg" };
-#ifdef __aarch64__
 const std::string DRAG_DROP_EXTENSION_SO_PATH { "/system/lib64/drag_drop_ext/libdrag_drop_ext.z.so" };
-#else
-const std::string DRAG_DROP_EXTENSION_SO_PATH { "/system/lib/drag_drop_ext/libdrag_drop_ext.z.so" };
-#endif
 const std::string BIG_FOLDER_LABEL { "scb_folder" };
 struct DrawingInfo g_drawingInfo;
 struct DragData g_dragData;
