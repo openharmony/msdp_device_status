bfff911380405121f7cd123c5f151b51f0c56dab
lwx1285820 <lishaoxiong10@huawei.com>
2024-04-28 08:48:17 +0000

    fix alarm pathPtr
    Signed-off-by: lwx1285820 <lishaoxiong10@huawei.com>

--- tools/vdev/src/virtual_device_builder.cpp
+++ tools/vdev/src/virtual_device_builder.cpp
@@ -394,19 +394,19 @@ void VirtualDeviceBuilder::WaitFor(const char *name, int32_t timeout)
 int32_t VirtualDeviceBuilder::ReadFile(const char *path, json &model)
 {
     CALL_DEBUG_ENTER;
-    std::string pathStr(path);
     CHKPR(path, RET_ERR);
     char realPath[PATH_MAX] {};
 
-    if (realpath(pathStr.c_str(), realPath) == nullptr) {
-        std::cout << "Invalid path: " << pathStr << std::endl;
+    if (realpath(path, realPath) == nullptr) {
+        std::cout << "Invalid path: " << path << std::endl;
         return RET_ERR;
     }
     if (Utility::GetFileSize(realPath) > MAXIMUM_FILESIZE_ALLOWED) {
         std::cout << "File is too large" << std::endl;
         return RET_ERR;
     }
-    std::cout << "Read input data from \'" << pathStr << "\'" << std::endl;
+    std::cout << "Read input data from \'" << realPath << "\'" << std::endl;
+    std::string pathStr(realPath);
     IfStreamWrap fileStream;
     fileStream.ifStream = std::ifstream(pathStr);
     if (!fileStream.IsOpen()) {
