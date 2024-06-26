79057bcf18f26565aa3b3931a15f18b17686f462
yang123 <yangyanling13@huawei.com>
2024-06-26 07:12:58 +0000

    update services/communication/service/src/devicestatus_srv_stub.cpp.
    Signed-off-by: yang123 <yangyanling13@huawei.com>

--- services/communication/service/src/devicestatus_srv_stub.cpp
+++ services/communication/service/src/devicestatus_srv_stub.cpp
@@ -53,92 +53,92 @@ void DeviceStatusSrvStub::InitCoordination()
     connFuncs_ = {
         { static_cast<uint32_t>(DeviceInterfaceCode::DEVICESTATUS_SUBSCRIBE),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->SubscribeStub(&data, &reply); 
-            } },
+                return this->SubscribeStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::DEVICESTATUS_UNSUBSCRIBE),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->UnsubscribeStub(&data, &reply); 
-            } },
+                return this->UnsubscribeStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::DEVICESTATUS_GETCACHE),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->GetLatestDeviceStatusDataStub(&data, &reply); 
-        } },
+                return this->GetLatestDeviceStatusDataStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::REGISTER_COORDINATION_MONITOR),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->RegisterCoordinationMonitorStub(&data, &reply); 
-        } },
+                return this->RegisterCoordinationMonitorStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::UNREGISTER_COORDINATION_MONITOR),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->UnregisterCoordinationMonitorStub(&data, &reply); 
-        } },
+                return this->UnregisterCoordinationMonitorStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::PREPARE_COORDINATION),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->PrepareCoordinationStub(&data, &reply); 
-        } },
+                return this->PrepareCoordinationStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::UNPREPARE_COORDINATION),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->UnPrepareCoordinationStub(&data, &reply); 
-        } },
+                return this->UnPrepareCoordinationStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::START_COORDINATION),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->ActivateCoordinationStub(&data, &reply); 
-        } },
+                return this->ActivateCoordinationStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::STOP_COORDINATION),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->DeactivateCoordinationStub(&data, &reply); 
-        } },
+                return this->DeactivateCoordinationStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::GET_COORDINATION_STATE),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->GetCoordinationStateStub(&data, &reply); 
-        } },
+                return this->GetCoordinationStateStub(data, reply); } 
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::GET_COORDINATION_STATE_SYNC),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->GetCoordinationStateSyncStub(&data, &reply); 
-        } },
+                return this->GetCoordinationStateSyncStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::ADD_HOT_AREA_MONITOR),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->AddHotAreaListenerStub(&data, &reply); 
-        } },
+                return this->AddHotAreaListenerStub(&data, &reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::REMOVE_HOT_AREA_MONITOR),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->RemoveHotAreaListenerStub(&data, &reply); 
-        } },
+                return this->RemoveHotAreaListenerStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::REGISTER_SUBSCRIPT_MONITOR),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->AddSubscriptListenerStub(&data, &reply); 
-        } },
+                return this->AddSubscriptListenerStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::UNREGISTER_SUBSCRIPT_MONITOR),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->RemoveSubscriptListenerStub(&data, &reply); 
-        } },
+                return this->RemoveSubscriptListenerStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::REGISTER_COOPERATE_MONITOR),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->RegisterCooperateMonitorStub(&data, &reply); 
-        } },
+                return this->RegisterCooperateMonitorStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::UNREGISTER_COOPERATE_MONITOR),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->UnregisterCooperateMonitorStub(&data, &reply); 
-        } },
+                return this->UnregisterCooperateMonitorStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::PREPARE_COOPERATE),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->PrepareCooperateStub(&data, &reply); 
-        } },
+                return this->PrepareCooperateStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::UNPREPARE_COOPERATE),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->UnPrepareCooperateStub(&data, &reply); 
-        } },
+                return this->UnPrepareCooperateStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::START_COOPERATE),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->ActivateCooperateStub(&data, &reply); 
-        } },
+                return this->ActivateCooperateStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::STOP_COOPERATE),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->DeactivateCooperateStub(&data, &reply); 
-        } },
+                return this->DeactivateCooperateStub(data, reply); }
+        },
         { static_cast<uint32_t>(DeviceInterfaceCode::GET_COOPERATE_STATE),
             [this](MessageParcel &data, MessageParcel &reply) {
-                return this->GetCooperateStateStub(&data, &reply); 
-        } }
+                return this->GetCooperateStateStub(data, reply); }
+        }
     };
 }
 
