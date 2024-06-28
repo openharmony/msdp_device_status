e3166a6791b622e460689512c0c7e77da7004620
lwx1285820 <lishaoxiong10@huawei.com>
2024-06-28 09:38:47 +0000

    CheckDeviceOnline and add unittest
    Signed-off-by: lwx1285820 <lishaoxiong10@huawei.com>

--- intention/adapters/dsoftbus_adapter/src/dsoftbus_adapter_impl.cpp
+++ intention/adapters/dsoftbus_adapter/src/dsoftbus_adapter_impl.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+1
 #include "dsoftbus_adapter_impl.h"
 
 #ifdef ENABLE_PERFORMANCE_CHECK
