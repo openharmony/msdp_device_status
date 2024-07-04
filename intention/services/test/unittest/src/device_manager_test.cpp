0a68f81543630c679443ae12c8d5eae3f8b418f6
李少雄 <lishaoxiong10@huawei.com>
2024-07-04 10:30:36 +0000

    update intention/services/test/unittest/src/device_manager_test.cpp.
    Signed-off-by: 李少雄 <lishaoxiong10@huawei.com>

--- intention/services/test/unittest/src/device_manager_test.cpp
+++ intention/services/test/unittest/src/device_manager_test.cpp
@@ -13,23 +13,9 @@
  * limitations under the License.
  */
 
-#include <gtest/gtest.h>
-#include <memory>
-#include <string>
+#include "device_manager_test.h"
 
-#include <fcntl.h>
-#include "nocopyable.h"
-
-#include "delegate_tasks.h"
-#include "device_manager.h"
-#include "devicestatus_define.h"
-#include "devicestatus_delayed_sp_singleton.h"
-#include "drag_manager.h"
-#include "i_context.h"
-#include "timer_manager.h"
-
-#include "intention_service.h"
-#include "socket_session_manager.h"
+#include <unistd.h>
 
 #undef LOG_TAG
 #define LOG_TAG "IntentionDeviceManagerTest"
@@ -37,54 +23,31 @@
 namespace OHOS {
 namespace Msdp {
 namespace DeviceStatus {
+
 using namespace testing::ext;
-class ContextService final : public IContext {
-    ContextService();
-    ~ContextService() = default;
-    DISALLOW_COPY_AND_MOVE(ContextService);
-public:
-    IDelegateTasks& GetDelegateTasks() override;
-    IDeviceManager& GetDeviceManager() override;
-    ITimerManager& GetTimerManager() override;
-    IDragManager& GetDragManager() override;
-    IPluginManager& GetPluginManager() override;
-    ISocketSessionManager& GetSocketSessionManager() override;
-    IInputAdapter& GetInput() override;
-    IDSoftbusAdapter& GetDSoftbus() override;
-private:
-    void OnStart();
-    bool Init();
-    __attribute__((no_sanitize("cfi"))) int32_t InitDeviceMgr();
-    int32_t InitDelegateTasks();
-    __attribute__((no_sanitize("cfi"))) static ContextService* GetInstance();
-private:
-    DelegateTasks delegateTasks_;
-    TimerManager timerMgr_;
-    DeviceManager devMgr_;
-    DragManager dragMgr_;
-    SocketSessionManager socketSessionMgr_;
-    std::unique_ptr<IInputAdapter> input_;
-    std::unique_ptr<IPluginManager> pluginMgr_;
-    std::unique_ptr<IDSoftbusAdapter> dsoftbusAda_;
-};
-class IntentionDeviceManagerTest : public testing::Test {
-public:
-    static void SetUpTestCase();
-    static void TearDownTestCase();
-    void SetUp();
-    void TearDown();
+namespace {
+struct device_status_epoll_event {
+    int32_t fd { -1 };
+    EpollEventType event_type { EPOLL_EVENT_BEGIN };
 };
 
-namespace {
-const std::string TEST_DEV_NODE { "/dev/input/TestDeviceNode" };
 ContextService *g_instance = nullptr;
 constexpr int32_t TIME_WAIT_FOR_OP_MS { 100 };
+constexpr int32_t DEFAULT_WAIT_TIME_MS { 1000 };
+constexpr int32_t WAIT_FOR_ONCE { 1 };
+constexpr int32_t MAX_N_RETRIES { 100 };
+const std::string TEST_DEV_NODE { "/dev/input/TestDeviceNode" };
 } // namespace
 
 ContextService::ContextService()
 {
     FI_HILOGI("OHOS_BUILD_ENABLE_INTENTION_FRAMEWORK is on");
     OnStart();
+}
+
+ContextService::~ContextService()
+{
+    OnStop();
 }
 
 IDelegateTasks& ContextService::GetDelegateTasks()
@@ -107,7 +70,7 @@ IDragManager& ContextService::GetDragManager()
     return dragMgr_;
 }
 
-__attribute__((no_sanitize("cfi"))) ContextService* ContextService::GetInstance()
+ContextService* ContextService::GetInstance()
 {
     static std::once_flag flag;
     std::call_once(flag, [&]() {
@@ -117,6 +80,7 @@ __attribute__((no_sanitize("cfi"))) ContextService* ContextService::GetInstance(
     });
     return g_instance;
 }
+
 ISocketSessionManager& ContextService::GetSocketSessionManager()
 {
     return socketSessionMgr_;
@@ -140,31 +104,54 @@ IDSoftbusAdapter& ContextService::GetDSoftbus()
 bool ContextService::Init()
 {
     CALL_DEBUG_ENTER;
+    if (EpollCreate() != RET_OK) {
+        FI_HILOGE("Create epoll failed");
+        return false;
+    }
     if (InitDelegateTasks() != RET_OK) {
         FI_HILOGE("Delegate tasks init failed");
         goto INIT_FAIL;
     }
-
-    if (InitDeviceMgr() != RET_OK) {
-        FI_HILOGE("DeviceMgr init failed");
+    if (InitTimerMgr() != RET_OK) {
+        FI_HILOGE("TimerMgr init failed");
+        goto INIT_FAIL;
+    }
+    if (InitDevMgr() != RET_OK) {
+        FI_HILOGE("DevMgr init failed");
         goto INIT_FAIL;
     }
 
     return true;
 
 INIT_FAIL:
+    EpollClose();
     return false;
 }
-
-__attribute__((no_sanitize("cfi"))) int32_t ContextService::InitDeviceMgr()
+int32_t ContextService::InitDevMgr()
 {
     CALL_DEBUG_ENTER;
     int32_t ret = devMgr_.Init(this);
     if (ret != RET_OK) {
-        FI_HILOGE("DeviceMgr init failed");
+        FI_HILOGE("DevMgr init failed");
         return ret;
     }
-    return RET_OK;
+    return ret;
+}
+
+int32_t ContextService::InitTimerMgr()
+{
+    CALL_DEBUG_ENTER;
+    int32_t ret = timerMgr_.Init(this);
+    if (ret != RET_OK) {
+        FI_HILOGE("TimerMgr init failed");
+        return ret;
+    }
+
+    ret = AddEpoll(EPOLL_EVENT_TIMER, timerMgr_.GetTimerFd());
+    if (ret != RET_OK) {
+        FI_HILOGE("AddEpoll for timer failed");
+    }
+    return ret;
 }
 
 int32_t ContextService::InitDelegateTasks()
@@ -174,16 +161,258 @@ int32_t ContextService::InitDelegateTasks()
         FI_HILOGE("The delegate task init failed");
         return RET_ERR;
     }
+    int32_t ret = AddEpoll(EPOLL_EVENT_ETASK, delegateTasks_.GetReadFd());
+    if (ret != RET_OK) {
+        FI_HILOGE("AddEpoll error ret:%{public}d", ret);
+    }
+    FI_HILOGI("AddEpoll, epollfd:%{public}d, fd:%{public}d", epollFd_, delegateTasks_.GetReadFd());
+    return ret;
+}
+
+int32_t ContextService::EpollCreate()
+{
+    CALL_DEBUG_ENTER;
+    epollFd_ = ::epoll_create1(EPOLL_CLOEXEC);
+    if (epollFd_ < 0) {
+        FI_HILOGE("epoll_create1 failed:%{public}s", ::strerror(errno));
+        return RET_ERR;
+    }
     return RET_OK;
+}
+
+int32_t ContextService::AddEpoll(EpollEventType type, int32_t fd)
+{
+    CALL_DEBUG_ENTER;
+    if (!(type >= EPOLL_EVENT_BEGIN && type < EPOLL_EVENT_END)) {
+        FI_HILOGE("Invalid type:%{public}d", type);
+        return RET_ERR;
+    }
+    if (fd < 0) {
+        FI_HILOGE("Invalid fd:%{public}d", fd);
+        return RET_ERR;
+    }
+    auto eventData = static_cast<device_status_epoll_event*>(malloc(sizeof(device_status_epoll_event)));
+    if (!eventData) {
+        FI_HILOGE("Malloc failed");
+        return RET_ERR;
+    }
+    eventData->fd = fd;
+    eventData->event_type = type;
+    FI_HILOGD("EventData:[fd:%{public}d, type:%{public}d]", eventData->fd, eventData->event_type);
+
+    struct epoll_event ev {};
+    ev.events = EPOLLIN;
+    ev.data.ptr = eventData;
+    if (EpollCtl(fd, EPOLL_CTL_ADD, ev) != RET_OK) {
+        free(eventData);
+        eventData = nullptr;
+        ev.data.ptr = nullptr;
+        FI_HILOGE("EpollCtl failed");
+        return RET_ERR;
+    }
+    return RET_OK;
+}
+
+int32_t ContextService::DelEpoll(EpollEventType type, int32_t fd)
+{
+    CALL_DEBUG_ENTER;
+    if (!(type >= EPOLL_EVENT_BEGIN && type < EPOLL_EVENT_END)) {
+        FI_HILOGE("Invalid type:%{public}d", type);
+        return RET_ERR;
+    }
+    if (fd < 0) {
+        FI_HILOGE("Invalid fd:%{public}d", fd);
+        return RET_ERR;
+    }
+    struct epoll_event ev {};
+    if (EpollCtl(fd, EPOLL_CTL_DEL, ev) != RET_OK) {
+        FI_HILOGE("DelEpoll failed");
+        return RET_ERR;
+    }
+    return RET_OK;
+}
+
+void ContextService::EpollClose()
+{
+    CALL_DEBUG_ENTER;
+    if (epollFd_ >= 0) {
+        if (close(epollFd_) < 0) {
+            FI_HILOGE("Close epoll fd failed, error:%{public}s, epollFd_:%{public}d", strerror(errno), epollFd_);
+        }
+        epollFd_ = -1;
+    }
+}
+
+int32_t ContextService::EpollCtl(int32_t fd, int32_t op, struct epoll_event &event)
+{
+    CALL_DEBUG_ENTER;
+    if (fd < 0) {
+        FI_HILOGE("Invalid fd:%{public}d", fd);
+        return RET_ERR;
+    }
+    if (epollFd_ < 0) {
+        FI_HILOGE("Invalid epollFd:%{public}d", epollFd_);
+        return RET_ERR;
+    }
+    if (::epoll_ctl(epollFd_, op, fd, &event) != 0) {
+        FI_HILOGE("epoll_ctl(%{public}d,%{public}d,%{public}d) failed:%{public}s", epollFd_, op, fd, ::strerror(errno));
+        return RET_ERR;
+    }
+    return RET_OK;
+}
+
+int32_t ContextService::EpollWait(int32_t maxevents, int32_t timeout, struct epoll_event &events)
+{
+    if (epollFd_ < 0) {
+        FI_HILOGE("Invalid epollFd:%{public}d", epollFd_);
+        return RET_ERR;
+    }
+    return epoll_wait(epollFd_, &events, maxevents, timeout);
+}
+
+void ContextService::OnTimeout(const struct epoll_event &ev)
+{
+    CALL_DEBUG_ENTER;
+    if ((ev.events & EPOLLIN) == EPOLLIN) {
+        uint64_t expiration {};
+        ssize_t ret = read(timerMgr_.GetTimerFd(), &expiration, sizeof(expiration));
+        if (ret < 0) {
+            FI_HILOGE("Read expiration failed:%{public}s", strerror(errno));
+        }
+        timerMgr_.ProcessTimers();
+    } else if ((ev.events & (EPOLLHUP | EPOLLERR)) != 0) {
+        FI_HILOGE("Epoll hangup:%{public}s", strerror(errno));
+    }
+}
+
+void ContextService::OnDeviceMgr(const struct epoll_event &ev)
+{
+    CALL_DEBUG_ENTER;
+    if ((ev.events & EPOLLIN) == EPOLLIN) {
+        devMgr_.Dispatch(ev);
+    } else if ((ev.events & (EPOLLHUP | EPOLLERR)) != 0) {
+        FI_HILOGE("Epoll hangup:%{public}s", strerror(errno));
+    }
+}
+
+int32_t ContextService::EnableDevMgr(int32_t nRetries)
+{
+    CALL_INFO_TRACE;
+    static int32_t timerId { -1 };
+    int32_t ret = devMgr_.Enable();
+    if (ret != RET_OK) {
+        FI_HILOGE("Failed to enable device manager");
+        if (nRetries > 0) {
+            timerId = timerMgr_.AddTimer(DEFAULT_WAIT_TIME_MS, WAIT_FOR_ONCE,
+                [this, nRetries] { return this->EnableDevMgr(nRetries - 1); });
+            if (timerId < 0) {
+                FI_HILOGE("AddTimer failed, Failed to enable device manager");
+            }
+        } else {
+            FI_HILOGE("Maximum number of retries exceeded, Failed to enable device manager");
+        }
+        return ret;
+    }
+    AddEpoll(EPOLL_EVENT_DEVICE_MGR, devMgr_.GetFd());
+    if (timerId >= 0) {
+        timerMgr_.RemoveTimer(timerId);
+        timerId = -1;
+    }
+    return RET_OK;
+}
+
+void ContextService::DisableDevMgr()
+{
+    DelEpoll(EPOLL_EVENT_DEVICE_MGR, devMgr_.GetFd());
+    devMgr_.Disable();
 }
 
 void ContextService::OnStart()
 {
     CALL_DEBUG_ENTER;
+    uint64_t tid = GetThisThreadId();
+    delegateTasks_.SetWorkerThreadId(tid);
+
     if (!Init()) {
         FI_HILOGE("On start call init failed");
         return;
     }
+    state_ = ServiceRunningState::STATE_RUNNING;
+    ready_ = true;
+
+    worker_ = std::thread(std::bind(&ContextService::OnThread, this));
+}
+
+void ContextService::OnStop()
+{
+    CALL_DEBUG_ENTER;
+    if (timerMgr_.GetTimerFd() >= 0) {
+        if (close(timerMgr_.GetTimerFd()) < 0) {
+            FI_HILOGE("Close timer fd failed, error:%{public}s", strerror(errno));
+        }
+    }
+    if (!ready_) {
+        FI_HILOGI("ready state is false");
+        return;
+    }
+    ready_ = false;
+    state_ = ServiceRunningState::STATE_EXIT;
+
+    delegateTasks_.PostAsyncTask([]() -> int32_t {
+        FI_HILOGD("No asynchronous operations");
+        return RET_OK;
+    });
+    if (worker_.joinable()) {
+        worker_.join();
+    }
+    DisableDevMgr();
+    EpollClose();
+    FI_HILOGI("OnStop leave");
+}
+
+void ContextService::OnThread()
+{
+    CALL_DEBUG_ENTER;
+    SetThreadName(std::string("os_ds_service"));
+    uint64_t tid = GetThisThreadId();
+    delegateTasks_.SetWorkerThreadId(tid);
+    EnableDevMgr(MAX_N_RETRIES);
+    FI_HILOGD("Main worker thread start, tid:%{public}" PRId64 "", tid);
+
+    while (state_ == ServiceRunningState::STATE_RUNNING) {
+        struct epoll_event ev[MAX_EVENT_SIZE] {};
+        int32_t count = EpollWait(MAX_EVENT_SIZE, -1, ev[0]);
+        for (int32_t i = 0; i < count && state_ == ServiceRunningState::STATE_RUNNING; i++) {
+            auto epollEvent = reinterpret_cast<device_status_epoll_event*>(ev[i].data.ptr);
+            CHKPC(epollEvent);
+            if (epollEvent->event_type == EPOLL_EVENT_TIMER) {
+                OnTimeout(ev[i]);
+            } else if (epollEvent->event_type == EPOLL_EVENT_ETASK) {
+                OnDelegateTask(ev[i]);
+            } else if (epollEvent->event_type == EPOLL_EVENT_DEVICE_MGR) {
+                OnDeviceMgr(ev[i]);
+            } else {
+                FI_HILOGW("Unknown epoll event type:%{public}d", epollEvent->event_type);
+            }
+        }
+    }
+    FI_HILOGD("Main worker thread stop, tid:%{public}" PRId64 "", tid);
+}
+
+void ContextService::OnDelegateTask(const struct epoll_event &ev)
+{
+    if ((ev.events & EPOLLIN) == 0) {
+        FI_HILOGW("Not epollin");
+        return;
+    }
+    DelegateTasks::TaskData data {};
+    ssize_t res = read(delegateTasks_.GetReadFd(), &data, sizeof(data));
+    if (res == -1) {
+        FI_HILOGW("Read failed erron:%{public}d", errno);
+    }
+    FI_HILOGD("RemoteRequest notify td:%{public}" PRId64 ", std:%{public}" PRId64 ""
+        ", taskId:%{public}d", GetThisThreadId(), data.tid, data.taskId);
+    delegateTasks_.ProcessTasks();
 }
 
 void IntentionDeviceManagerTest::SetUpTestCase() {}
@@ -199,7 +428,7 @@ void IntentionDeviceManagerTest::TearDown() {}
 
 /**
  * @tc.name: IntentionDeviceManagerTest01
- * @tc.desc: Test Init and Enable
+ * @tc.desc: Test the founction AddDevice
  * @tc.type: FUNC
  */
 HWTEST_F(IntentionDeviceManagerTest, IntentionDeviceManagerTest01, TestSize.Level1)
@@ -207,143 +436,96 @@ HWTEST_F(IntentionDeviceManagerTest, IntentionDeviceManagerTest01, TestSize.Leve
     CALL_TEST_DEBUG;
     auto env = ContextService::GetInstance();
     ASSERT_NE(env, nullptr);
-    int32_t ret = env->devMgr_.Enable();
-    EXPECT_NE(ret, RET_OK);
-    ret = env->devMgr_.Disable();
-    EXPECT_NE(ret, RET_OK);
+    env->devMgr_.RemoveDevice(TEST_DEV_NODE);
+    env->devMgr_.FindDevice(TEST_DEV_NODE);
+    auto ret = env->devMgr_.AddDevice(TEST_DEV_NODE);
+    ASSERT_EQ(ret, nullptr);
+    ret = env->devMgr_.FindDevice(TEST_DEV_NODE);
+    ASSERT_EQ(ret, nullptr);
+    ret = env->devMgr_.RemoveDevice(TEST_DEV_NODE);
+    ASSERT_EQ(ret, nullptr);
 }
 
 /**
  * @tc.name: IntentionDeviceManagerTest02
- * @tc.desc: test ParseDeviceId
+ * @tc.desc: Test the founction Dispatch
  * @tc.type: FUNC
- * @tc.require:
  */
 HWTEST_F(IntentionDeviceManagerTest, IntentionDeviceManagerTest02, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
     auto env = ContextService::GetInstance();
     ASSERT_NE(env, nullptr);
-    std::string devNode = "event123";
-    int32_t expectedDeviceId = 123;
-    int32_t actualDeviceId = env->devMgr_.ParseDeviceId(devNode);
-    EXPECT_EQ(actualDeviceId, expectedDeviceId);
-    devNode = "invalid_device";
-    actualDeviceId = env->devMgr_.ParseDeviceId(devNode);
-    EXPECT_EQ(actualDeviceId, RET_ERR);
+    auto eventData = static_cast<device_status_epoll_event*>(malloc(sizeof(device_status_epoll_event)));
+    eventData->fd = 1;
+    eventData->event_type = EPOLL_EVENT_BEGIN;
+    struct epoll_event ev {};
+    ev.events = EPOLLIN;
+    ev.data.ptr = eventData;
+    ASSERT_NO_FATAL_FAILURE(env->devMgr_.Dispatch(ev));
 }
 
 /**
  * @tc.name: IntentionDeviceManagerTest03
- * @tc.desc: test FindDevice
+ * @tc.desc: Test the founction GetDevice
  * @tc.type: FUNC
- * @tc.require:
  */
 HWTEST_F(IntentionDeviceManagerTest, IntentionDeviceManagerTest03, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
     auto env = ContextService::GetInstance();
     ASSERT_NE(env, nullptr);
-    env->devMgr_.AddDevice(TEST_DEV_NODE);
-    std::shared_ptr<IDevice> foundDevice = env->devMgr_.FindDevice(TEST_DEV_NODE);
-    EXPECT_EQ(foundDevice, nullptr);
-    std::shared_ptr<IDevice> removeDevice = env->devMgr_.RemoveDevice(TEST_DEV_NODE);
-    EXPECT_EQ(removeDevice, nullptr);
+    auto ret = env->GetDeviceManager().GetDevice(1);
+    ASSERT_EQ(ret, nullptr);
 }
+
 
 /**
  * @tc.name: IntentionDeviceManagerTest04
- * @tc.desc: test Dispatch
+ * @tc.desc: Test the founction RetriggerHotplug
  * @tc.type: FUNC
- * @tc.require:
  */
 HWTEST_F(IntentionDeviceManagerTest, IntentionDeviceManagerTest04, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
     auto env = ContextService::GetInstance();
     ASSERT_NE(env, nullptr);
-    uint32_t events = EPOLLIN | EPOLLOUT;
-    struct epoll_event ev {};
-    ev.events = events;
-    ev.data.ptr = nullptr;
-    ASSERT_NO_FATAL_FAILURE(env->devMgr_.Dispatch(ev));
-    int32_t ret = env->devMgr_.OnEpollDispatch(events);
-    EXPECT_NE(ret, RET_OK);
+    std::weak_ptr<IDeviceObserver> weakObserver = std::weak_ptr<IDeviceObserver>();
+    ASSERT_NO_FATAL_FAILURE(env->GetDeviceManager().RetriggerHotplug(weakObserver));
 }
 
 /**
  * @tc.name: IntentionDeviceManagerTest05
- * @tc.desc: test GetDevice
+ * @tc.desc: Test the founction AddDeviceObserver
  * @tc.type: FUNC
- * @tc.require:
  */
 HWTEST_F(IntentionDeviceManagerTest, IntentionDeviceManagerTest05, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
     auto env = ContextService::GetInstance();
     ASSERT_NE(env, nullptr);
-    std::shared_ptr<IDevice> ret = env->devMgr_.GetDevice(1);
-    EXPECT_EQ(ret, nullptr);
-    std::shared_ptr<IDevice> result = env->devMgr_.OnGetDevice(1);
-    EXPECT_EQ(result, nullptr);
+    std::weak_ptr<IDeviceObserver> weakObserver = std::weak_ptr<IDeviceObserver>();
+    int32_t ret = env->GetDeviceManager().AddDeviceObserver(weakObserver);
+    EXPECT_EQ(ret, RET_ERR);
+    ASSERT_NO_FATAL_FAILURE(env->GetDeviceManager().RemoveDeviceObserver(weakObserver));
 }
 
 /**
  * @tc.name: IntentionDeviceManagerTest06
- * @tc.desc: test RetriggerHotplug
+ * @tc.desc: Test the founction GetKeyboard
  * @tc.type: FUNC
- * @tc.require:
  */
 HWTEST_F(IntentionDeviceManagerTest, IntentionDeviceManagerTest06, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
     auto env = ContextService::GetInstance();
     ASSERT_NE(env, nullptr);
-    std::weak_ptr<IDeviceObserver> weakObserver = std::weak_ptr<IDeviceObserver>();
-    ASSERT_NO_FATAL_FAILURE(env->devMgr_.RetriggerHotplug(weakObserver));
-    int32_t ret = env->devMgr_.OnRetriggerHotplug(weakObserver);
-    EXPECT_NE(ret, RET_OK);
+    env->devMgr_.AddDevice(TEST_DEV_NODE);
+    std::vector<std::shared_ptr<IDevice>> keyboards;
+    keyboards = env->devMgr_.GetKeyboard();
+    bool ret = env->devMgr_.HasKeyboard();
+    ASSERT_FALSE(ret);
 }
-
-/**
- * @tc.name: IntentionDeviceManagerTest07
- * @tc.desc: test AddDeviceObserver
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(IntentionDeviceManagerTest, IntentionDeviceManagerTest07, TestSize.Level1)
-{
-    CALL_TEST_DEBUG;
-    auto env = ContextService::GetInstance();
-    ASSERT_NE(env, nullptr);
-    std::weak_ptr<IDeviceObserver> weakObserver = std::weak_ptr<IDeviceObserver>();
-    int32_t ret = env->devMgr_.AddDeviceObserver(weakObserver);
-    EXPECT_NE(ret, RET_OK);
-    int32_t tmp = env->devMgr_.OnAddDeviceObserver(weakObserver);
-    EXPECT_NE(tmp, RET_OK);
-    ASSERT_NO_FATAL_FAILURE(env->devMgr_.RemoveDeviceObserver(weakObserver));
-    int32_t result = env->devMgr_.OnRemoveDeviceObserver(weakObserver);
-    EXPECT_NE(result, RET_OK);
-}
-
-/**
- * @tc.name: IntentionDeviceManagerTest08
- * @tc.desc: test AnyOf
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(IntentionDeviceManagerTest, IntentionDeviceManagerTest08, TestSize.Level1)
-{
-    CALL_TEST_DEBUG;
-    auto env = ContextService::GetInstance();
-    ASSERT_NE(env, nullptr);
-    auto pred = [](std::shared_ptr<IDevice> device) {
-        return device != nullptr;
-    };
-    EXPECT_FALSE(env->devMgr_.AnyOf(pred));
-}
-
-
 } // namespace DeviceStatus
 } // namespace Msdp
 } // namespace OHOS
