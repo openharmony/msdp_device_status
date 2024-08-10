f51753752bdc438fc2c2d39764b0d1092ae606b1
yang123 <yangyanling13@huawei.com>
2024-08-10 02:28:46 +0000

    add unittest
    Signed-off-by: yang123 <yangyanling13@huawei.com>

--- test/unittest/intentiontest/src/drag_server_test.cpp
+++ test/unittest/intentiontest/src/drag_server_test.cpp
@@ -24,6 +24,9 @@
 #include "ipc_skeleton.h"
 #include "singleton.h"
 #include "tunnel_client.h"
+#include "accesstoken_kit.h"
+#include "nativetoken_kit.h"
+#include "token_setproc.h"
 
 namespace OHOS {
 namespace Msdp {
@@ -70,6 +73,19 @@ std::shared_ptr<DragServer> g_dragServerOne { nullptr };
 IContext *g_context { nullptr };
 IContext *g_contextOne { nullptr };
 std::shared_ptr<TunnelClient> g_tunnel { nullptr };
+Security::AccessToken::HapInfoParams g_testInfoParms = {
+    .userID = 1,
+    .bundleName = "drag_server_test",
+    .instIndex = 0,
+    .appIDDesc = "test"
+};
+
+Security::AccessToken::HapPolicyParams g_testPolicyPrams = {
+    .apl = Security::AccessToken::APL_NORMAL,
+    .domain = "test.domain",
+    .permList = {},
+    .permStateList = {}
+};
 } // namespace
 
 ContextService::ContextService()
@@ -212,6 +228,26 @@ std::optional<DragData> DragServerTest::CreateDragData(int32_t sourceType,
     return dragData;
 }
 
+uint64_t NativeTokenGet()
+{
+    uint64_t tokenId;
+    NativeTokenInfoParams infoInstance = {
+        .dcapsNum = 0,
+        .permsNum = 0,
+        .aclsNum = 0,
+        .dcaps = nullptr,
+        .perms = nullptr,
+        .acls = nullptr,
+        .aplStr = "system_basic",
+    };
+
+    infoInstance.processName = " DragServerTest";
+    tokenId = GetAccessTokenId(&infoInstance);
+    SetSelfTokenID(tokenId);
+    FI_HILOGI(" tokenId:%{public}lu", tokenId);
+    OHOS::Security::AccessToken::AccessTokenKit::ReloadNativeTokenInfo();
+    return tokenId;
+}
 class TestStartDragListener : public IStartDragListener {
 public:
     explicit TestStartDragListener(std::function<void(const DragNotifyMsg&)> function) : function_(function) { }
@@ -933,7 +969,7 @@ HWTEST_F(DragServerTest, DragServerTest28, TestSize.Level0)
     int32_t ret = param.Marshalling(datas);
     EXPECT_EQ(ret, READ_OK);
     ret = g_dragServer->Stop(context, datas, reply);
-    EXPECT_EQ(ret, RET_ERR);
+    EXPECT_EQ(ret, RET_OK);
     g_dragMgr.dragState_ = DragState::STOP;
 }
 
@@ -1367,6 +1403,57 @@ HWTEST_F(DragServerTest, DragServerTest47, TestSize.Level0)
     MessageParcel datas;
     int32_t ret = g_dragServer->SetDragWindowScreenId(context, datas, reply);
     EXPECT_EQ(ret, RET_ERR);
+}
+
+/**
+ * @tc.name: DragServerTest48
+ * @tc.desc: Drag Drawing
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(DragServerTest, DragServerTest48, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    uint64_t g_tokenId = NativeTokenGet();
+    EXPECT_EQ(g_tokenId, IPCSkeleton::GetCallingTokenID());
+    CallingContext context {
+        .intention = g_intention,
+        .tokenId = IPCSkeleton::GetCallingTokenID(),
+        .uid = IPCSkeleton::GetCallingUid(),
+        .pid = IPCSkeleton::GetCallingPid(),
+    };
+    MessageParcel reply;
+    MessageParcel datas;
+    g_dragServer->GetPackageName(IPCSkeleton::GetCallingTokenID());
+    bool ret = g_dragServer->IsSystemHAPCalling(context);
+    EXPECT_TRUE(ret);
+    OHOS::Security::AccessToken::AccessTokenKit::DeleteToken(g_tokenId);
+}
+
+/**
+ * @tc.name: DragServerTest49
+ * @tc.desc: Drag Drawing
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(DragServerTest, DragServerTest49, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    Security::AccessToken::AccessTokenIDEx tokenIdEx = {0};
+    tokenIdEx = Security::AccessToken::AccessTokenKit::AllocHapToken(g_testInfoParms, g_testPolicyPrams);
+    EXPECT_EQ(0, SetSelfTokenID(tokenIdEx.tokenIdExStruct.tokenID));
+    auto g_tokenId1 = tokenIdEx.tokenIdExStruct.tokenID;
+    CallingContext context {
+        .intention = g_intention,
+        .tokenId = g_tokenId1,
+        .uid = IPCSkeleton::GetCallingUid(),
+        .pid = IPCSkeleton::GetCallingPid(),
+    };
+    MessageParcel reply;
+    MessageParcel datas;
+    g_dragServer->GetPackageName(g_tokenId1);
+    bool ret = g_dragServer->IsSystemHAPCalling(context);
+    EXPECT_FALSE(ret);
 }
 } // namespace DeviceStatus
 } // namespace Msdp
