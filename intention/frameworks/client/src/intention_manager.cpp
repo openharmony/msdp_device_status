078da62d883ce963db9a9b1f1cf7b0f88fdc4966
pengyanggit <yangpeng177@huawei.com>
2024-09-02 18:35:07 +0800

    Signed-off-by: pengyanggit yangpeng177@huawei.com
    Signed-off-by: pengyanggit <yangpeng177@huawei.com>
    Change-Id: Ib925b9e66eadd289263eb0a361e068cda01697ca

--- intention/frameworks/client/src/intention_manager.cpp
+++ intention/frameworks/client/src/intention_manager.cpp
@@ -27,8 +27,8 @@ namespace OHOS {
 namespace Msdp {
 namespace DeviceStatus {
 namespace {
-constexpr int32_t INDEX_MAIN { 0 };
-constexpr int32_t INDEX_FULL { 1 };
+constexpr int32_t INDEX_FOLDED { 0 };
+constexpr int32_t INDEX_EXPAND { 1 };
 constexpr size_t POLICY_VEC_SIZE { 2 };
 const std::string SCREEN_ROTATION { "1" };
 } // namespace
@@ -432,15 +432,10 @@ int32_t IntentionManager::RotateDragWindowSync(const std::shared_ptr<Rosen::RSTr
             return drag_.RotateDragWindowSync(*tunnel_, rsTransaction);
         }
         Rosen::FoldStatus foldStatus = Rosen::DisplayManager::GetInstance().GetFoldStatus();
-        if (((foldStatus == Rosen::FoldStatus::EXPAND) && (foldRotatePolicys_[INDEX_FULL] == SCREEN_ROTATION)) ||
-            ((foldStatus == Rosen::FoldStatus::FOLDED) && (foldRotatePolicys_[INDEX_MAIN] == SCREEN_ROTATION))) {
-            if (foldRotatePolicys_[INDEX_FULL] == SCREEN_ROTATION) {
-                FI_HILOGD("Full display rotation, not need rotate drag window");
-                return RET_OK;
-            } else if (foldRotatePolicys_[INDEX_MAIN] == SCREEN_ROTATION) {
-                FI_HILOGD("Main display rotation, not need rotate drag window");
-                return RET_OK;
-            }
+        if (((foldStatus == Rosen::FoldStatus::EXPAND) && (foldRotatePolicys_[INDEX_EXPAND] == SCREEN_ROTATION)) ||
+            ((foldStatus == Rosen::FoldStatus::FOLDED) && (foldRotatePolicys_[INDEX_FOLDED] == SCREEN_ROTATION))) {
+            FI_HILOGD("Full display rotation, not need rotate drag window");
+            return RET_OK;
         }
     }
     return drag_.RotateDragWindowSync(*tunnel_, rsTransaction);
