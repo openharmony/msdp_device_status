6b4e28e91066b5660acc46024975813d0be1c150
yang123 <yangyanling13@huawei.com>
2024-10-25 03:10:28 +0000

    空指针校验
    Signed-off-by: yang123 <yangyanling13@huawei.com>

--- services/interaction/drag/src/display_change_event_listener.cpp
+++ services/interaction/drag/src/display_change_event_listener.cpp
@@ -58,6 +58,7 @@ void DisplayChangeEventListener::OnChange(Rosen::DisplayId displayId)
             lastRotation_ = Rosen::Rotation::ROTATION_0;
             CHKPV(context_);
             int32_t ret = context_->GetDelegateTasks().PostAsyncTask([this] {
+                CHKPR(context_, RET_ERR);
                 return this->context_->GetDragManager().RotateDragWindow(Rosen::Rotation::ROTATION_0);
             });
             if (ret != RET_OK) {
@@ -96,6 +97,7 @@ void DisplayChangeEventListener::RotateDragWindow(Rosen::Rotation rotation)
 {
     CHKPV(context_);
     int32_t ret = context_->GetDelegateTasks().PostAsyncTask([this, rotation] {
+        CHKPR(context_, RET_ERR);
         return this->context_->GetDragManager().RotateDragWindow(rotation);
     });
     if (ret != RET_OK) {
@@ -107,6 +109,7 @@ void DisplayChangeEventListener::ScreenRotate(Rosen::Rotation rotation, Rosen::R
 {
     CHKPV(context_);
     int32_t ret = context_->GetDelegateTasks().PostAsyncTask([this, rotation, lastRotation] {
+        CHKPR(context_, RET_ERR);
         return this->context_->GetDragManager().ScreenRotate(rotation, lastRotation);
     });
     if (ret != RET_OK) {
