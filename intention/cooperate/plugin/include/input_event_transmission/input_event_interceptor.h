5a6bb353888e979df68688ec541d3983ea0021aa
yang123 <yangyanling13@huawei.com>
2024-11-19 03:04:04 +0000

    update intention/cooperate/plugin/include/input_event_transmission/input_event_interceptor.h.
    Signed-off-by: yang123 <yangyanling13@huawei.com>

--- intention/cooperate/plugin/include/input_event_transmission/input_event_interceptor.h
+++ intention/cooperate/plugin/include/input_event_transmission/input_event_interceptor.h
@@ -56,15 +56,12 @@ private:
     IContext *env_ { nullptr };
     int32_t interceptorId_ { -1 };
     bool scanState_ { true };
+    std::atomic<int32_t> heartTimer_ { -1 };
     int32_t pointerEventTimer_ { -1 };
-    std::atomic_bool heartSwitch_ { true };
-    int32_t heartTimer_ { -1 };
-    std::atomic<int32_t> heartBeatRunning_ { true };
     std::string remoteNetworkId_;
     Channel<CooperateEvent>::Sender sender_;
     static std::set<int32_t> filterKeys_;
     static std::set<int32_t> filterPointers_;
-    std::thread thread_;
 };
 } // namespace Cooperate
 } // namespace DeviceStatus
