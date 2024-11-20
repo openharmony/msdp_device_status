284c6b83c53238716b7851ecaf82f8e80d230f5b
pengyanggit <yangpeng177@huawei.com>
2024-11-20 14:06:23 +0800

    Signed-off-by: pengyanggit yangpeng177@huawei.com
    Signed-off-by: pengyanggit <yangpeng177@huawei.com>
    Change-Id: I1fb2fe0af722971bd6501dd265ee3b96f664697a

--- services/interaction/drag/src/drag_manager.cpp
+++ services/interaction/drag/src/drag_manager.cpp
@@ -437,8 +437,8 @@ int32_t DragManager::StopDrag(const DragDropResult &dropResult, const std::strin
     dragResult_ = static_cast<DragResult>(dropResult.result);
 #endif // OHOS_BUILD_ENABLE_ARKUI_X
     SetDragState(DragState::STOP);
-    if (isControlMultiScreenVisible_) {
-        isControlMultiScreenVisible_ = false;
+    if (GetControlCollaborationVisible()) {
+        SetControlCollaborationVisible(false);
     }
 #ifndef OHOS_BUILD_ENABLE_ARKUI_X
     ReportStopDragRadarInfo(BizState::STATE_END, StageRes::RES_SUCCESS, DragRadarErrCode::DRAG_SUCCESS, pid,
@@ -1045,8 +1045,8 @@ int32_t DragManager::OnStartDrag(const std::string &packageName, int32_t pid)
     FI_HILOGI("enter");
     pullId_ = GenerateId();
     FI_HILOGI("Current pullId:%{public}d", pullId_.load());
-    if (isControlMultiScreenVisible_) {
-        isControlMultiScreenVisible_ = false;
+    if (GetControlCollaborationVisible()) {
+        SetControlCollaborationVisible(false);
     }
     auto extraData = CreateExtraData(true);
     DragData dragData = DRAG_DATA_MGR.GetDragData();
@@ -1163,7 +1163,7 @@ int32_t DragManager::OnSetDragWindowVisible(bool visible, bool isForce)
 #endif // OHOS_BUILD_ENABLE_ARKUI_X
         return RET_ERR;
     }
-    if (!isForce && isControlMultiScreenVisible_) {
+    if (!isForce && GetControlCollaborationVisible()) {
         FI_HILOGW("The drag-and-drop window is controlled by multi-screen coordination,"
             "can not set drag window visible:%{public}d", visible);
         return RET_OK;
@@ -1183,7 +1183,7 @@ int32_t DragManager::OnSetDragWindowVisible(bool visible, bool isForce)
 #endif // OHOS_BUILD_ENABLE_ARKUI_X
     }
     if (isForce) {
-        isControlMultiScreenVisible_ = isForce;
+        SetControlCollaborationVisible(isForce);
         FI_HILOGW("The drag-and-drop window is controlled by multi-screen coordination");
     }
     return RET_OK;
@@ -1293,6 +1293,15 @@ void DragManager::SetDragOriginDpi(float dragOriginDpi)
 DragResult DragManager::GetDragResult() const
 {
     return dragResult_;
+}
+
+void SetControlCollaborationVisible(bool visible)
+{
+    isControlCollaborationVisible_ = visible;
+}
+bool GetControlCollaborationVisible() const
+{
+    return isControlCollaborationVisible_;
 }
 
 int32_t DragManager::GetDragSummary(std::map<std::string, int64_t> &summarys)
