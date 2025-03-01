c6787c06c75ccf5ec7e37046e72ae5930c311de1
yuxiaoyang <yuxiaoyang6@hisilicon.com>
2025-03-01 07:34:35 +0000

    回旋镖接口1
    Signed-off-by: yuxiaoyang <yuxiaoyang6@hisilicon.com>

--- services/native/include/devicestatus_napi_manager.h
+++ services/native/include/devicestatus_napi_manager.h
@@ -1,8 +1,23 @@
+/*
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DEVICESTATUS_NAPI_MANAGER_H
+#define DEVICESTATUS_NAPI_MANAGER_H
+
 #include <string>
 #include "pixel_map.h"
-
-#ifndef BOOMERANG_NAPI_H
-#define BOOMERANG_NAPI_H
 
 namespace OHOS {
 namespace Msdp {
@@ -31,9 +46,7 @@ public:
                             std::shared_ptr<Media::PixelMap> &resultPixelMap);
     static void DecodeImage(std::shared_ptr<Media::PixelMap> &pixelMap, std::string &content);
 };
-
 }
 }
 }
-
-#endif // BOOMERANG_NAPI_H
+#endif // DEVICESTATUS_NAPI_MANAGER_H
