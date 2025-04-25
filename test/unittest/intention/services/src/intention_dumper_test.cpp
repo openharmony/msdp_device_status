989cfbb02d733cdad1d8cd4ffcdb79e4f7c86a6e
yang123 <yangyanling13@huawei.com>
2025-04-25 03:25:30 +0000

    update test/unittest/intention/services/src/intention_dumper_test.cpp.
    Signed-off-by: yang123 <yangyanling13@huawei.com>

--- test/unittest/intention/services/src/intention_dumper_test.cpp
+++ test/unittest/intention/services/src/intention_dumper_test.cpp
@@ -25,6 +25,15 @@
 #include "devicestatus_define.h"
 #include "devicestatus_errors.h"
 #include "stationary_server.h"
+#include "intention_service_test.h"
+#include "ddm_adapter.h"
+#include "dsoftbus_adapter.h"
+#include "fi_log.h"
+#include "input_adapter.h"
+#include "intention_service.h"
+#include "interaction_manager.h"
+#include "ipc_skeleton.h"
+#include "plugin_manager.h"
 
 #undef LOG_TAG
 #define LOG_TAG "IntentionDumperTest"
@@ -34,6 +43,7 @@ namespace Msdp {
 namespace DeviceStatus {
 using namespace testing::ext;
 namespace {
+std::shared_ptr<ContextService> g_context { nullptr };
 int32_t g_fd { 1 };
 StationaryServer stationary;
 constexpr int32_t TIME_WAIT_FOR_OP_MS { 20 };
@@ -58,6 +68,78 @@ void IntentionDumperTest::TearDown()
     std::this_thread::sleep_for(std::chrono::milliseconds(TIME_WAIT_FOR_OP_MS));
 }
 
+int32_t MockDelegateTasks::PostSyncTask(DTaskCallback callback)
+{
+    return callback();
+}
+
+int32_t MockDelegateTasks::PostAsyncTask(DTaskCallback callback)
+{
+    return callback();
+}
+
+ContextService* ContextService::GetInstance()
+{
+    static std::once_flag flag;
+    std::call_once(flag, [&]() {
+        g_context = std::make_shared<ContextService>();
+    });
+    return g_context.get();
+}
+
+ContextService::ContextService()
+{
+    ddm_ = std::make_unique<DDMAdapter>();
+    input_ = std::make_unique<InputAdapter>();
+    pluginMgr_ = std::make_unique<PluginManager>(this);
+    dsoftbus_ = std::make_unique<DSoftbusAdapter>();
+}
+
+IDelegateTasks& ContextService::GetDelegateTasks()
+{
+    return delegateTasks_;
+}
+
+IDeviceManager& ContextService::GetDeviceManager()
+{
+    return devMgr_;
+}
+
+ITimerManager& ContextService::GetTimerManager()
+{
+    return timerMgr_;
+}
+
+IDragManager& ContextService::GetDragManager()
+{
+    return dragMgr_;
+}
+
+ISocketSessionManager& ContextService::GetSocketSessionManager()
+{
+    return socketSessionMgr_;
+}
+
+IDDMAdapter& ContextService::GetDDM()
+{
+    return *ddm_;
+}
+
+IPluginManager& ContextService::GetPluginManager()
+{
+    return *pluginMgr_;
+}
+
+IInputAdapter& ContextService::GetInput()
+{
+    return *input_;
+}
+
+IDSoftbusAdapter& ContextService::GetDSoftbus()
+{
+    return *dsoftbus_;
+}
+
 /**
  * @tc.name: IntentionDumperTest001
  * @tc.desc: test Intention Dumper
@@ -69,7 +151,7 @@ HWTEST_F(IntentionDumperTest, IntentionDumperTest001, TestSize.Level1)
     CALL_TEST_DEBUG;
     IContext *env { nullptr };
     IntentionDumper dumper = IntentionDumper(env, stationary);
-    std::vector<std::string> argList;
+    std::vector<std::string> argList = {"s", "h", "l", "c", "d"};
     ASSERT_NO_FATAL_FAILURE(dumper.Dump(g_fd, argList));
 }
 
@@ -82,7 +164,8 @@ HWTEST_F(IntentionDumperTest, IntentionDumperTest001, TestSize.Level1)
 HWTEST_F(IntentionDumperTest, IntentionDumperTest002, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    IContext *env { nullptr };
+    auto env = ContextService::GetInstance();
+    ASSERT_NE(env, nullptr);
     IntentionDumper dumper = IntentionDumper(env, stationary);
     int32_t value = 's';
     ASSERT_NO_FATAL_FAILURE(dumper.DumpOnce(g_fd, value));
@@ -97,7 +180,8 @@ HWTEST_F(IntentionDumperTest, IntentionDumperTest002, TestSize.Level1)
 HWTEST_F(IntentionDumperTest, IntentionDumperTest003, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    IContext *env { nullptr };
+    auto env = ContextService::GetInstance();
+    ASSERT_NE(env, nullptr);
     IntentionDumper dumper = IntentionDumper(env, stationary);
     int32_t value = 'l';
     ASSERT_NO_FATAL_FAILURE(dumper.DumpOnce(g_fd, value));
@@ -112,7 +196,8 @@ HWTEST_F(IntentionDumperTest, IntentionDumperTest003, TestSize.Level1)
 HWTEST_F(IntentionDumperTest, IntentionDumperTest004, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    IContext *env { nullptr };
+    auto env = ContextService::GetInstance();
+    ASSERT_NE(env, nullptr);
     IntentionDumper dumper = IntentionDumper(env, stationary);
     int32_t value = 'c';
     ASSERT_NO_FATAL_FAILURE(dumper.DumpOnce(g_fd, value));
@@ -127,7 +212,8 @@ HWTEST_F(IntentionDumperTest, IntentionDumperTest004, TestSize.Level1)
 HWTEST_F(IntentionDumperTest, IntentionDumperTest005, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    IContext *env { nullptr };
+    auto env = ContextService::GetInstance();
+    ASSERT_NE(env, nullptr);
     IntentionDumper dumper = IntentionDumper(env, stationary);
     int32_t value = 'd';
     ASSERT_NO_FATAL_FAILURE(dumper.DumpOnce(g_fd, value));
@@ -142,7 +228,8 @@ HWTEST_F(IntentionDumperTest, IntentionDumperTest005, TestSize.Level1)
 HWTEST_F(IntentionDumperTest, IntentionDumperTest006, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    IContext *env { nullptr };
+    auto env = ContextService::GetInstance();
+    ASSERT_NE(env, nullptr);
     IntentionDumper dumper = IntentionDumper(env, stationary);
     int32_t value = 'm';
     ASSERT_NO_FATAL_FAILURE(dumper.DumpOnce(g_fd, value));
@@ -157,7 +244,8 @@ HWTEST_F(IntentionDumperTest, IntentionDumperTest006, TestSize.Level1)
 HWTEST_F(IntentionDumperTest, IntentionDumperTest007, TestSize.Level1)
 {
     CALL_TEST_DEBUG;
-    IContext *env { nullptr };
+    auto env = ContextService::GetInstance();
+    ASSERT_NE(env, nullptr);
     IntentionDumper dumper = IntentionDumper(env, stationary);
 
     int32_t value = 'y';
