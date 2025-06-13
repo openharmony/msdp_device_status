aafb202bdedb29aa7dc2dec7a233f6b5c3f0b743
yang123 <yangyanling13@huawei.com>
2025-06-13 08:23:02 +0000

    update frameworks/js/napi/interaction/coordination/src/js_coordination_context.cpp.
    Signed-off-by: yang123 <yangyanling13@huawei.com>

--- frameworks/js/napi/interaction/coordination/src/js_coordination_context.cpp
+++ frameworks/js/napi/interaction/coordination/src/js_coordination_context.cpp
@@ -213,7 +213,7 @@ napi_value JsCoordinationContext::ActivateCooperateWithOptions(napi_env env, nap
     CHKRP(napi_get_value_int32(env, argv[1], &startDeviceId), GET_VALUE_INT32);
 
     if (!UtilNapi::TypeOf(env, argv[ARGV_TWO], napi_object)) {
-        FI_HILOGI("CooperateOptions is not assined，call ActiovateCooperate.");
+        FI_HILOGI("CooperateOptions is not assigned，call ActivateCooperate.");
         bool isCompatible = true;
         return jsCoordinationMgr->Activate(env, remoteNetworkId, startDeviceId, isCompatible);
     }
