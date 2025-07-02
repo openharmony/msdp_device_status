71bd54834b0bf3f31fb43818c327d8bc8619d681
就是铸币捏 <tianruifeng@huawei.com>
2025-07-02 17:02:30 +0800

    fix compile err
    Signed-off-by: 就是铸币捏 <tianruifeng@huawei.com>

--- test/unittest/interfaces/src/devicestatus_client_test.cpp
+++ test/unittest/interfaces/src/devicestatus_client_test.cpp
@@ -203,8 +203,6 @@ HWTEST_F(DeviceStatusClientTest, GetPageContent001, TestSize.Level0)
     option.contentUnderstand = true;
     option.pageLink = true;
     option.textOnly = true;
-    option.longTextSplit = true;
-    option.elementHook = true;
     OnScreen::PageContent pageContent;
     int32_t ret = OnScreen::OnScreenManager::GetInstance()->GetPageContent(option, pageContent);
     std::cout << pageContent.windowId << ", " << pageContent.bundleName << ", "
