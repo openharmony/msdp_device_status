bb7f8b81d54cbdd1dcfd13acac1167df081ad589
lcaidm <lichen139@huawei.com>
2025-07-24 07:19:23 +0000

    update frameworks/js/napi/motion/src/napi_event_utils.cpp.
    Signed-off-by: lcaidm <lichen139@huawei.com>

--- frameworks/js/napi/motion/src/napi_event_utils.cpp
+++ frameworks/js/napi/motion/src/napi_event_utils.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Copyright (C) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,3 +13,72 @@
  * limitations under the License.
  */
 
+
+
+#include "napi_event_utils.h"
+
+#include "app_event.h"
+#include "app_event_processor_mgr.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace {
+const std::string SDK_NAME = "MultimodalAwarenessKit";
+constexpr int32_t TIMEOUT = 90; // 每90s触发上报一次
+constexpr int32_t ROW = 30; // 或30条数据触发一次上报
+}
+
+int64_t NapiEventUtils::AddProcessor()
+{
+    HiviewDFX::HiAppEvent::ReportConfig config;
+    config.name = "ha_app_event"; // 系统预制so，实现上报功能，由HA提供
+    config.appId = "com_huawei_hmos_sdk_ocg";
+    config.routeInfo = "AUTO";
+    config.triggerCond.timeout = TIMEOUT;
+    config.triggerCond.row = ROW;
+    config.eventConfigs.clear();
+    {   // 固定内容，所有Kit全量完整复制，不允许修改
+        OHOS::HiviewDFX::HiAppEvent::EventConfig event1;
+        event1.domain = "api_diagnostic";
+        event1.name = "api_exec_end";
+        event1.isRealTime = false;
+        config.eventConfigs.push_back(event1);
+    }
+    {   // 固定内容，所有Kit全量完整复制，不允许修改
+        OHOS::HiviewDFX::HiAppEvent::EventConfig event2;
+        event2.domain = "api_diagnostic";
+        event2.name = "api_called_stat";
+        event2.isRealTime = true;
+        config.eventConfigs.push_back(event2);
+    }
+    {   // 固定内容，所有Kit全量完整复制，不允许修改
+        OHOS::HiviewDFX::HiAppEvent::EventConfig event3;
+        event3.domain = "api_diagnostic";
+        event3.name = "api_called_stat_cnt";
+        event3.isRealTime = true;
+        config.eventConfigs.push_back(event3);
+    }
+    return HiviewDFX::HiAppEvent::AppEventProcessorMgr::AddProcessor(config);
+}
+
+void NapiEventUtils::WriteEndEvent(const std::string& transId, const std::string& apiName, const int64_t beginTime,
+    const int result, const int errCode)
+{
+    HiviewDFX::HiAppEvent::Event event("api_diagnostic", "api_exec_end", HiviewDFX::HiAppEvent::BEHAVIOR);
+    event.AddParam("trans_id", transId);
+    event.AddParam("api_name", apiName);
+    event.AddParam("sdk_name", SDK_NAME);
+    event.AddParam("begin_time", beginTime);
+    event.AddParam("end_time", GetSysClockTime());
+    event.AddParam("result", result);
+    event.AddParam("error_code", errCode);
+    Write(event);
+}
+
+int64_t NapiEventUtils::GetSysClockTime()
+{
+    return std::chrono::time_point_cast<std::chrono::milliseconds>(
+            std::chrono::steady_clock::now()).time_since_epoch().count();
+}
+} // namespace Msdp
+} // namespace OHOS
