c18b69baeba327e1b5ce446fbf44ffd7fa532a8c
KangPeng <kangpeng18@h-partners.com>
2025-12-22 19:27:03 +0800

    arkts function back
    Signed-off-by: KangPeng <kangpeng18@h-partners.com>

--- frameworks/ets/metadataBinding/src/ani_constructor.cpp
+++ frameworks/ets/metadataBinding/src/ani_constructor.cpp
@@ -25,12 +25,18 @@
 ANI_EXPORT ani_status ANI_Constructor(ani_vm *vm, uint32_t *result)
 {
     ani_env *env;
-    if (ANI_OK != vm->GetEnv(ANI_VERSION_1, &env)) {
-        FI_HILOGE("Error from vm->GetEnv");
+    if (vm == nullptr || result == nullptr) {
+        FI_HILOGE("vm or result is null");
         return ANI_ERROR;
     }
-    if (ANI_OK != ohos::multimodalAwareness::metadataBinding::ANIRegister(env)) {
-        FI_HILOGE("Error from ohos::multimodalAwareness::metadataBinding::ANIRegister");
+    ani_status status = vm->GetEnv(ANI_VERSION_1, &env);
+    if (status != ANI_OK) {
+        FI_HILOGE("Error from vm->GetEnv, status:%{public}d", status);
+        return ANI_ERROR;
+    }
+    status = ohos::multimodalAwareness::metadataBinding::ANIRegister(env);
+    if (status != ANI_OK) {
+        FI_HILOGE("Error from ohos::multimodalAwareness::metadataBinding::ANIRegister, status:%{public}d", status);
         return ANI_ERROR;
     }
     *result = ANI_VERSION_1;
