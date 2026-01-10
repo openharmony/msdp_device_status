e77b4dc7863b09b42994e868a977b071aae0b1ef
xing-jiangpan2 <xingjiangpan2@huawei.com>
2026-01-10 12:41:12 +0800

    Increases drag coverage
    Signed-off-by: xing-jiangpan2 <xingjiangpan2@huawei.com>

--- test/unittest/intention/drag/include/test_context.h
+++ test/unittest/intention/drag/include/test_context.h
@@ -85,8 +85,6 @@ class TestContext final : public IContext {
 public:
     TestContext();
     ~TestContext() = default;
-
-
     IDelegateTasks& GetDelegateTasks() override;
     IDeviceManager& GetDeviceManager() override;
     ITimerManager& GetTimerManager() override;
