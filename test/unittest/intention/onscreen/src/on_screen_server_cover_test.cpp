3d7d241a2e4de82d1e0eaa046ef865b68af27b4c
yang-kaka <yangke105@huawei.com>
2026-01-27 11:20:09 +0800

    log check code
    Signed-off-by: yang-kaka <yangke105@huawei.com>

--- test/unittest/intention/onscreen/src/on_screen_server_cover_test.cpp
+++ test/unittest/intention/onscreen/src/on_screen_server_cover_test.cpp
@@ -74,7 +74,7 @@ AccessTokenID GetNativeTokenIdFromProcess(const std::string &process)
     AccessTokenKit::DumpTokenInfo(info, dumpInfo);
     size_t pos = dumpInfo.find("\"tokenID\": ");
     if (pos == std::string::npos) {
-        FI_HILOGE("tokenid not find");
+        FI_HILOGE("tokenID not find");
         return 0;
     }
     pos += std::string("\"tokenID\": ").length();
@@ -217,6 +217,407 @@ void OnScreenServerTest::SetUp()
 void OnScreenServerTest::TearDown()
 {
     onScreen_.handle_.pAlgorithm = nullptr;
+}
+
+void OnScreenServerTest::SetUpTestCase()
+{
+    g_shellTokenId = GetSelfTokenID();
+    SetTestEvironment(g_shellTokenId);
+    std::vector<std::string> reqPerm;
+    reqPerm.emplace_back(PERMISSION_GET_PAGE_CONTENT);
+    reqPerm.emplace_back(PERMISSION_SEND_CONTROL_EVENT);
+    g_mock = new (std::nothrow) MockHapToken("OnScreenServerTest", reqPerm, true);
+    CHKPV(g_mock);
+    FI_HILOGI("SetUpTestCase ok.");
+}
+
+void OnScreenServerTest::TearDownTestCase()
+{
+    if (g_mock != nullptr) {
+        delete g_mock;
+        g_mock = nullptr;
+    }
+    std::lock_guard<std::mutex> lock(g_lockSetToken);
+    g_shellTokenId = 0;
+    SetSelfTokenID(g_shellTokenId);
+}
+
+void OnScreenServerTest::OnScreenServerTestCallback::OnScreenChange(const std::string& changeInfo)
+{
+    FI_HILOGI("OnScreenChange");
+}
+
+/**
+ * @tc.name: IsParallelFeatureEnabled001
+ * @tc.desc: Test func named IsParallelFeatureEnabled
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, IsParallelFeatureEnabled001, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    CallingContext context{
+        .tokenId = IPCSkeleton::GetCallingTokenID(),
+        .uid = IPCSkeleton::GetCallingUid(),
+        .pid = IPCSkeleton::GetCallingPid(),
+    };
+    int32_t windowId = 1;
+    int32_t outStatus = 0;
+    int32_t ret = onScreen_.IsParallelFeatureEnabled(context, windowId, outStatus);
+    EXPECT_EQ(ret, RET_NO_SUPPORT);
+}
+
+/**
+ * @tc.name: GetLiveStatus001
+ * @tc.desc: Test func named GetLiveStatus
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, GetLiveStatus001, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    int32_t ret = onScreen_.GetLiveStatus();
+    EXPECT_EQ(ret, RET_NO_SUPPORT);
+}
+
+/**
+ * @tc.name: GetAppIdentifier001
+ * @tc.desc: Test func named GetAppIdentifier
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, GetAppIdentifier001, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    std::string bundleName = "OnScreenServerTest";
+    int32_t userId = 0;
+    std::string appIdentifier = "";
+    bool ret = onScreen_.GetAppIdentifier(bundleName, userId, appIdentifier);
+    EXPECT_FALSE(ret);
+}
+
+/**
+ * @tc.name: IsWhitelistAppCalling001
+ * @tc.desc: Test func named IsWhitelistAppCalling
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, IsWhitelistAppCalling001, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    CallingContext context{
+        .tokenId = IPCSkeleton::GetCallingTokenID(),
+        .uid = IPCSkeleton::GetCallingUid(),
+        .pid = IPCSkeleton::GetCallingPid(),
+    };
+    bool ret =  onScreen_.IsWhitelistAppCalling(context);
+    EXPECT_FALSE(ret);
+}
+
+/**
+ * @tc.name: FillDumpCommonData001
+ * @tc.desc: Test func named FillDumpCommonData
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpCommonData001, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    OnscreenAwarenessInfo info;
+    onScreen_.FillDumpCommonData(info);
+    EXPECT_EQ(info.bundleName, std::string("com.ohos.duoxi"));
+}
+
+/**
+ * @tc.name: FillDumpScenarioTodo001
+ * @tc.desc: Test func named FillDumpScenarioTodo
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData001, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo.empty());
+}
+
+/**
+ * @tc.name: FillDumpScenarioTodo002
+ * @tc.desc: Test func named FillDumpScenarioTodo
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData002, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    cap.capList.push_back("contentUiTree");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_FALSE(wholeInfo.entityInfo.empty());
+}
+
+/**
+ * @tc.name: FillDumpData003
+ * @tc.desc: Test func named FillDumpData
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData003, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    cap.capList.push_back("contentUiTree");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("uiTree") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+}
+
+/**
+ * @tc.name: FillDumpData004
+ * @tc.desc: Test func named FillDumpContentUiTreeWithTree
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData004, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    cap.capList.push_back("contentUiTreeWithImage");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("imagesCompID") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+}
+
+/**
+ * @tc.name: FillDumpData005
+ * @tc.desc: Test func named FillDumpOcr
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData005, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    cap.capList.push_back("contentUiOcr");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("uiTree") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+}
+
+/**
+ * @tc.name: FillDumpData006
+ * @tc.desc: Test func named FillDumpData
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData006, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    cap.capList.push_back("contentScreenshot");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("screenshotID") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+}
+
+/**
+ * @tc.name: FillDumpData007
+ * @tc.desc: Test func named FillDumpData
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData007, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    cap.capList.push_back("contentLink");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("pagLink") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+}
+
+/**
+ * @tc.name: FillDumpData008
+ * @tc.desc: Test func named FillDumpData
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData008, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    cap.capList.push_back("interactionClick");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("uiTree") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+}
+
+/**
+ * @tc.name: FillDumpData009
+ * @tc.desc: Test func named FillDumpData
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData009, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    cap.capList.push_back("interactionScroll");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("uiTree") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+}
+
+/**
+ * @tc.name: FillDumpData010
+ * @tc.desc: Test func named FillDumpData
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData010, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    cap.capList.push_back("interactionTextSelection");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("textSelection") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+}
+
+/**
+ * @tc.name: FillDumpData011
+ * @tc.desc: Test func named FillDumpData
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData011, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    cap.capList.push_back("scenarioReading");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("title") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("content") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("wordCount") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("lazyLoad") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+}
+
+/**
+ * @tc.name: FillDumpData012
+ * @tc.desc: Test func named FillDumpData
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData012, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    std::map<std::string, ValueObj> entityInfo;
+    cap.capList.push_back("scenarioShortVideo");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("category") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("hasBackGroundMusic") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("imagID") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("publisher") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("description") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("series") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("hotSearch") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("SearchTips") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("position") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+}
+
+/**
+ * @tc.name: FillDumpData013
+ * @tc.desc: Test func named FillDumpData
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData013, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    std::map<std::string, ValueObj> entityInfo;
+    cap.capList.push_back("interactionTextSelection");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("textSelection") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+}
+
+/**
+ * @tc.name: FillDumpData014
+ * @tc.desc: Test func named FillDumpData
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, FillDumpData014, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    AwarenessCap cap;
+    std::map<std::string, ValueObj> entityInfo;
+    cap.capList.push_back("scenarioTodo");
+    AwarenessOptions option;
+    OnscreenAwarenessInfo wholeInfo = onScreen_.FillDumpData(cap, option);
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("homeworkAssign") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("homeworkName") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("chatgroupName") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("pageName") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("subject") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("assigntime") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("deadline") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("description") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+    ASSERT_TRUE(wholeInfo.entityInfo[0].entityInfo.find("teacherName") !=
+                wholeInfo.entityInfo[0].entityInfo.end());
+}
+
+/**
+ * @tc.name: NotifyClient001
+ * @tc.desc: Test func named NotifyClient
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, NotifyClient001, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    ASSERT_NO_FATAL_FAILURE(onScreen_.NotifyClient());
+}
+
+/**
+ * @tc.name: OnScreenShotIntent001
+ * @tc.desc: Test func named OnScreenShotIntent
+ * @tc.type: FUNC
+ */
+HWTEST_F(OnScreenServerTest, OnScreenShotIntent001, TestSize.Level1)
+{
+    CALL_TEST_DEBUG;
+    CallingContext context{
+        .tokenId = IPCSkeleton::GetCallingTokenID(),
+        .uid = IPCSkeleton::GetCallingUid(),
+        .pid = IPCSkeleton::GetCallingPid(),
+    };
+    AwarenessOptions option;
+    OnscreenAwarenessInfo info;
+    int32_t ret = onScreen_.OnScreenShotIntent(context, option, info);
+    EXPECT_EQ(ret, RET_PARAM_ERR);
+
+    option.entityInfo.insert(std::make_pair("windowId", 1));
+    option.entityInfo.insert(std::make_pair("image", 1));
+    ret = onScreen_.OnScreenShotIntent(context, option, info);
+    EXPECT_EQ(ret, RET_PARAM_ERR);
 }
 
 } // namespace OnScreen
