ba31aff43377dbb3a915817905564735d4d72b0a
hyt-8693 <huiyuantong8@h-partners.com>
2026-01-29 08:38:14 +0800

    UT_msdp_kit
    Signed-off-by: hyt-8693 <huiyuantong8@h-partners.com>

--- test/unittest/intention/ipc/src/msdp_kit_report_new_test.cpp
+++ test/unittest/intention/ipc/src/msdp_kit_report_new_test.cpp
@@ -19,315 +19,182 @@
 #include <limits>
 #include <string>
 #include <functional>
-#include <memory>
+#include "msdp_timer_info.h"
+
+#undef LOG_TAG
+#define LOG_TAG "MsdpTimerUnitTest"
 
 namespace OHOS {
 namespace Msdp {
-
-#undef LOG_TAG
-#define LOG_TAG "MsdpKitReportUnitTest"
 using namespace testing::ext;
+namespace {
+constexpr int32_t TIMER_TYPE_WAKEUP = 1 << 1;
+constexpr int32_t TIMER_TYPE_EXACT = 1 << 2;
+constexpr int64_t INTERVAL_HOUR = 12 * 60 * 60;
+constexpr int64_t INTERVAL_12_HOURS = 12 * 60 * 60 * 1000;
+constexpr int64_t MS_PER_SEC = 1000;
+constexpr int32_t ONE_HOUR_SECONDS = 3600;
+} // namespace
 
-class ITimerInfo {
+class TimerUtilTest : public testing::Test {
 public:
-    virtual ~ITimerInfo() = default;
-    virtual void OnTrigger() = 0;
-    virtual void SetCallback(std::function<void()>) = 0;
-    static constexpr int TIMER_TYPE_REALTIME = 1 << 0;
-    static constexpr int TIMER_TYPE_REALTIME_WAKEUP = 1 << 1
-    static constexpr int TIMER_TYPE_EXACT = 1 << 2;
-    static constexpr int TIMER_TYPE_IDLE = 1 << 3;
-
-private:
-    static constexpr int g_timerTypeRealtime 1 << 0;
-    static constexpr int g_timerTypeRealtimeWakeup = 1 << 1;
-    static constexpr int g_timerTypeExact = 1 << 2;
-    static constexpr int g_timerTypeIdle = 1 << 3;
-};
-
-class MsdpTimerInfo : public MiscServices::ITimerInfo
-{
-public:
-    MsdpTimer = default;
-    ~MsdpTimerInfo() override = default;
-
-    std::string Ts2Str(uint64_t timestamp) 
-    {
-        class MsdpTimerInfo : public MiscServices::ITimerInfo
-        time_t time = static_cast<time_t>(timestamp);
-        struct tm timeInfo = {};
-        struct tm* result = gmtime_r(&time, &timeInfo);
-        if (result == nullptr) {
-            return "Invalid time";
-        }
-        char buffer[80] = {0};
-        size_t ret = strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &timeInfo);
-        if (ret == 0) {
-            return "Invalid time format";
-        }
-        return std::string(buffer);
-    }
-    
-    void OnTrigger() override
-    {
-        if (callback_) {
-            callback_();
-        }
-    }
-}
-void SetCallback(std::function<void()> callback) override
-    {
-        callback_ = callback;
-    }
-
-    explicit MsdpTimerInfo(const dpTimerInfo& other)
-    {
-        callback_ = other.callback_;
-    }
-
-    MsdpTimerInfo& operator=(const MsdpTimerInfo& other)
-    {
-        if (this != &other) {
-            callback_ = other.callback_;
-        }
-        return *this;
-    }
-    MsdpTimerInfo(MsdpTimerInfo&& other) noexcept : callback_(std::move(other.callback_)) {}
-    MsdpTimerInfo& operator=(MsdpTimerInfo&& other) noexcept
-    {
-        if (this != &other) {
-            callback_ = std::move(other.callback_);
-        }
-        return *this;
-    }
-    
-private:
-    std::function<void()> callback_;
-};
-
-class MsdpKitReport {
-public:
-    static MsdpKitReport& GetInstance()
-    {
-        static MsdpKitReport instance;
-        return instance;
-    }
-    void AddEventProcessor() {
-    }
-    void MsdpInterfaceEventReport() {
-    }
-    void SchedulerUpload() {
-    }
-private:
-    MsdpKitReport() = default;
-    ~MsdpKitReport() = default;
-    MsdpKitReport(const MsdpKitReport&) = delete;
-    MsdpKitReport& operator=(const MsdpKitReport&) = delete;
-};
-
-class MockTimeFunctions {
-public:
-virtual ~MockTimeFunctions() = default;
-    virtual struct tm* GmtimeR(const time_t* timep, struct tm* result)
-    {
-        return ::gmtime_r(timep, result);
-    }
-    virtual size_t Strftime(char* str, size_t count, const char* format, const struct tm* time)
-    {
-        return ::strftime(str, count, format, time)
-    }
-    virtual size_t Strftime(char* str, size_t count, const char* format, const struct tm* time)
-    {
-};
-
-class MsdpKitReportUnitTest : public testing::Test {
-public:
-    static void SetUpTestCase()
-    {
-        MsdpKitReport::GetInstance().AddEventProcessor();
-    };
-    
-    static void TearDownTestCase(){};
-    
-    void SetUp()
-    {
-        mockTimeFunctions_ = std::make_unique<MockTimeFunctions>();
-    };
-    
-    void TearDown()
-    {
-        mockTimeFunctions_.reset();
-    };
-    
-    std::unique_ptr<MockTimeFunctions> mockTimeFunctions_;
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    void SetUp() override {}
+    void TearDown() override {}
 };
 
 /**
- * @tc.name: TestMsdpTimerInfo
- * @tc.desc: Test MsdpTimerInfo_Normal_Test_001
+ * @tc.name: TimeStamp_To_String_Normal_Test_001
+ * @tc.desc: Test normal timestamp conversion
  * @tc.type: FUNC
  */
-HWTEST_F(MsdpKitReportUnitTest, MsdpTimerInfo_Normal_Test_001, TestSize.Level1)
+HWTEST_F(TimerUtilTest, TimeStamp_To_String_Normal_Test_001, TestSize.Level1)
 {
-    constexpr uint64_t kValidTimeStamp = 1700000000;
-    MsdpTimerInfo msdpTimerInfo;
-    auto str = msdpTimerInfo.Ts2Str(VALID_TIME_STAMP);
-    EXPECT_NE(str, "Invalid time");
-    EXPECT_NE(str, "Invalid time format");
-    EXPECT_GT(str.length(), 0);
-    EXPECT_LT(str.length(), 255);
-    EXPECT_NE(str.find("-"), std::string::npos);
-    EXPECT_NE(str.find(":"), std::string::npos);
+    constexpr uint64_t validTimestamp = 1700000000;
+    MsdpTimerInfo timer;
+    auto timeStr = timer.Ts2Str(validTimestamp);
+    EXPECT_FALSE(timeStr.empty());
+    EXPECT_NE(timeStr, "Invalid time");
+    EXPECT_NE(timeStr, "Invalid time format");
 }
 
 /**
- * @tc.name: TestMsdpTimerInfo
- * @tc.desc: Test MsdpTimerInfo_MaxTime_Test_002
+ * @tc.name: TimeStamp_To_String_Zero_Test_002
+ * @tc.desc: Test zero timestamp conversion
  * @tc.type: FUNC
  */
-HWTEST_F(MsdpKitReportUnitTest, MsdpTimerInfo_MaxTime_Test_002, TestSize.Level1)
+HWTEST_F(TimerUtilTest, TimeStamp_To_String_Zero_Test_002, TestSize.Level1)
 {
-    MsdpTimerInfo msdpTimerInfo;
-    auto maxTimeT = std::numeric_limits<time_t>::max();
-    auto str = msdpTimerInfo.Ts2Str(static_cast<uint64_t>(maxTimeT));
-    EXPECT_TRUE(str == "Invalid time" ||
-                (str.find("-") != std::string::npos && str.find(":") != std::string::npos));
+    MsdpTimerInfo timer;
+    auto timeStr = timer.Ts2Str(0);
+    EXPECT_FALSE(timeStr.empty());
+    EXPECT_NE(timeStr, "Invalid time");
+    EXPECT_NE(timeStr, "Invalid time format");
 }
 
 /**
- * @tc.name: TestMsdpTimerInfo
- * @tc.desc: Test MsdpTimerInfo_ZeroTimestamp_Test_003
+ * @tc.name: TimeStamp_To_String_Recent_Test_003
+ * @tc.desc: Test recent timestamp conversion
  * @tc.type: FUNC
  */
-HWTEST_F(MsdpKitReportUnitTest, MsdpTimerInfo_ZeroTimestamp_Test_003, TestSize.Level1)
+HWTEST_F(TimerUtilTest, TimeStamp_To_String_Recent_Test_003, TestSize.Level1)
 {
-    MsdpTimerInfo msdpTimerInfo;
-    auto str = msdpTimerInfo.Ts2Str(0);
-    EXPECT_NE(str, "Invalid time");
-    EXPECT_NE(str, "Invalid time format");
-    EXPECT_NE(str.find("1970"), std::string::npos);
+    MsdpTimerInfo timer;
+    time_t current = time(nullptr);
+    auto timeStr = timer.Ts2Str(current);
+    EXPECT_FALSE(timeStr.empty());
+    EXPECT_NE(timeStr, "Invalid time");
+
+    auto futureTime = current + ONE_HOUR_SECONDS;
+    timeStr = timer.Ts2Str(futureTime);
+    EXPECT_FALSE(timeStr.empty());
 }
 
 /**
- * @tc.name: TestMsdpTimerInfo
- * @tc.desc: Test MsdpTimerInfo_NoCallback_Test_004
+ * @tc.name: Timer_Constants_Test_004
+ * @tc.desc: Test timer constant values
  * @tc.type: FUNC
  */
-HWTEST_F(MsdpKitReportUnitTest, MsdpTimerInfo_OnTrigger_NoCallback_Test_004, TestSize.Level1)
+HWTEST_F(TimerUtilTest, Timer_Constants_Test_004, TestSize.Level1)
 {
-    MsdpTimerInfo msdpTimerInfo;
-    EXPECT_NO_FATAL_FAILURE(msdpTimerInfo.OnTrigger());
+    EXPECT_EQ(TIMER_TYPE_WAKEUP, 2);
+    EXPECT_EQ(TIMER_TYPE_EXACT, 4);
+    EXPECT_EQ(INTERVAL_HOUR, 43200);
+    EXPECT_EQ(MS_PER_SEC, 1000);
+    EXPECT_EQ(TIMER_TYPE_WAKEUP | TIMER_TYPE_EXACT, 6);
 }
 
 /**
- * @tc.name: TestMsdpTimerInfo
- * @tc.desc: Test MsdpTimerInfo_WithCallback_Test_005
+ * @tc.name: Timer_Type_Settings_Test_006
+ * @tc.desc: Test timer type settings
  * @tc.type: FUNC
  */
-HWTEST_F(MsdpKitReportUnitTest, MsdpTimerInfo_WithCallback_Test_005, TestSize.Level1)
+HWTEST_F(TimerUtilTest, Timer_Type_Settings_Test_006, TestSize.Level1)
 {
-    MsdpTimerInfo msdpTimerInfo;
-    bool callbackCalled = false;
-    msdpTimerInfo.SetCallback([&callbackCalled]() {
-        callbackCalled = true;
-    });
-    msdpTimerInfo.OnTrigger();
-    EXPECT_TRUE(callbackCalled);
+    MsdpTimerInfo timer;
+
+    EXPECT_NO_FATAL_FAILURE(timer.SetType(TIMER_TYPE_WAKEUP));
+    EXPECT_NO_FATAL_FAILURE(timer.SetType(TIMER_TYPE_EXACT));
+    EXPECT_NO_FATAL_FAILURE(timer.SetType(TIMER_TYPE_WAKEUP | TIMER_TYPE_EXACT));
+    EXPECT_NO_FATAL_FAILURE(timer.SetRepeat(true));
+    EXPECT_NO_FATAL_FAILURE(timer.SetRepeat(false));
+    EXPECT_NO_FATAL_FAILURE(timer.SetInterval(INTERVAL_12_HOURS));
+    EXPECT_NO_FATAL_FAILURE(timer.SetInterval(0));
+    EXPECT_NO_FATAL_FAILURE(timer.SetInterval(1000));
 }
 
 /**
- * @tc.name: TestMsdpTimerInfo
- * @tc.desc: Test MsdpTimerInfo_InvalidFormat_Test_006
+ * @tc.name: Timer_Invalid_Timestamp_Test_007
+ * @tc.desc: Test invalid timestamp conversion
  * @tc.type: FUNC
  */
-HWTEST_F(MsdpKitReportUnitTest, MsdpTimerInfoInvalidFormat_Test_006, TestSize.Level1)
+HWTEST_F(TimerUtilTest, Timer_Invalid_Timestamp_Test_007, TestSize.Level1)
 {
-    MsdpTimerInfo msdpTimerInfo;
-    auto str = msdpTimerInfo.Ts2Str(1609459200);
-    EXPECT_NE(str, "Invalid time format");
+    MsdpTimerInfo timer;
+    constexpr uint64_t invalidTimestamp = 0xFFFFFFFFFFFFFFFF;
+    auto timeStr = timer.Ts2Str(invalidTimestamp);
+    EXPECT_TRUE(timeStr == "Invalid time" || timeStr == "Invalid time format");
 }
 
 /**
- * @tc.name: TestMsdpTimerInfo
- * @tc.desc: Test MsdpKitReport_Singleton_Test_007
+ * @tc.name: Timer_Multiple_Instances_Test_008
+ * @tc.desc: Test multiple timer instances
  * @tc.type: FUNC
  */
-HWTEST_F(MsdpKitReportUnitTest, MsdpKitReport_Singleton_Test_007, TestSize.Level1)
-{
-    auto& instance1 = MsdpKitReport::GetInstance();
-    auto& instance2 = MsdpKitReport::GetInstance();
-    EXPECT_EQ(&instance1, &instance2);
-    EXPECT_NO_FATAL_FAILURE(instance1.MsdpInterfaceEventReport());
-}
-
-/**
- * @tc.name: TestMsdpTimerInfo
- * @tc.desc: Test MsdpTimerInfo_CopyAndMove_Test_008
- * @tc.type: FUNC
- */
-HWTEST_F(MsdpKitReportUnitTest, MsdpTimerInfo_CopyAndMove_Test_008, TestSize.Level1)
+HWTEST_F(TimerUtilTest, Timer_Multiple_Instances_Test_008, TestSize.Level1)
 {
     MsdpTimerInfo timer1;
-    bool callback1Called = false;
-    timer1.SetCallback([&callback1Called]() {
-        callback1Called = true;
+    MsdpTimerInfo timer2;
+    
+    bool timer1CallbackCalled = false;
+    bool timer2CallbackCalled = false;
+    
+    timer1.SetCallback([&timer1CallbackCalled]() {
+        timer1CallbackCalled = true;
     });
-    MsdpTimerInfo timer2(timer1);
-    bool callback2Called = false;
-    timer2.SetCallback([&callback2Called]() {
-        callback2Called = true;
+    
+    timer2.SetCallback([&timer2CallbackCalled]() {
+        timer2CallbackCalled = true;
     });
-    MsdpTimerInfo timer3;
-    timer3 = timer1;
-    MsdpTimerInfo timer4(std::move(timer1));
-    MsdpTimerInfo timer5;
-    timer5 = std::move(timer2);
-    EXPECT_NO_FATAL_FAILURE(timer3.OnTrigger());
-    EXPECT_NO_FATAL_FAILURE(timer4.OnTrigger());
-    EXPECT_NO_FATAL_FAILURE(timer5.OnTrigger());
-    constexpr uint64_t kValidTimeStamp = 1700000000;
-    auto str = timer3.Ts2Str(kValidTimeStamp);
-    EXPECT_NE(str, "Invalid time");
+    
+    timer1.OnTrigger();
+    EXPECT_TRUE(timer1CallbackCalled);
+    EXPECT_FALSE(timer2CallbackCalled);
+    
+    timer2.OnTrigger();
+    EXPECT_TRUE(timer2CallbackCalled);
 }
 
 /**
- * @tc.name: TestMsdpTimerInfo
- * @tc.desc: Test MsdpTimerInfo_Ts2Str_NegativeTimestamp_Test_009
+ * @tc.name: Timer_Same_Timestamp_Test_009
+ * @tc.desc: Test same timestamp conversion
  * @tc.type: FUNC
  */
-HWTEST_F(MsdpKitReportUnitTest, MsdpTimerInfo_Ts2Str_NegativeTimestamp_Test_009, TestSize.Level1)
+HWTEST_F(TimerUtilTest, Timer_Same_Timestamp_Test_009, TestSize.Level1)
 {
-    MsdpTimerInfo msdpTimerInfo;
-    auto str = msdpTimerInfo.Ts2Str(1);
-    EXPECT_NE(str, "Invalid time");
-    EXPECT_NE(str, "Invalid time format");
+    MsdpTimerInfo timer1;
+    MsdpTimerInfo timer2;
+    constexpr uint64_t testTimestamp = 1609459200;
+    
+    auto timeStr1 = timer1.Ts2Str(testTimestamp);
+    auto timeStr2 = timer2.Ts2Str(testTimestamp);
+    
+    EXPECT_EQ(timeStr1, timeStr2);
+    EXPECT_FALSE(timeStr1.empty());
 }
 
 /**
- * @tc.name: TestMsdpTimerInfo
- * @tc.desc: Test MsdpKitReport_AddEventProcessor_Test_010
+ * @tc.name: Timer_Max_Timestamp_Test_010
+ * @tc.desc: Test max timestamp conversion
  * @tc.type: FUNC
  */
-HWTEST_F(MsdpKitReportUnitTest, MsdpKitReport_AddEventProcessor_Test_010, TestSize.Level1)
+HWTEST_F(TimerUtilTest, Timer_Max_Timestamp_Test_010, TestSize.Level1)
 {
-    EXPECT_NO_FATAL_FAILURE(MsdpKitReport::GetInstance().AddEventProcessor());
-    EXPECT_NO_FATAL_FAILURE(MsdpKitReport::GetInstance().SchedulerUpload());
-}
-
-/**
- * @tc.name: TestMsdpTimerInfo
- * @tc.desc: Test MsdpTimerInfo_Constants_Test_011
- * @tc.type: FUNC
- */
-HWTEST_F(MsdpKitReportUnitTest, MsdpTimerInfo_Constants_Test_011, TestSize.Level1)
-{
-    constexpr int32_t NON_APP_PROCESSOR_ID = -200;
-    constexpr uint64_t TIME_STAMP = 0xFFFFFFFF;
-    EXPECT_EQ(NON_APP_PROCESSOR_ID, -200);
-    EXPECT_EQ(TIME_STAMP, 0xFFFFFFFF);
-    MsdpTimerInfo msdpTimerInfo;
-    auto str = msTimerInfo.Ts2Str(TIME_STAMP);
-    EXPECT_FALSE(str.empty());
+    MsdpTimerInfo timer;
+    auto maxTimeT = std::numeric_limits<time_t>::max();
+    auto timeStr = timer.Ts2Str(static_cast<uint64_t>(maxTimeT));
+    EXPECT_TRUE(timeStr == "Invalid time" || !timeStr.empty());
 }
 }  // namespace Msdp
 }  // namespace OHOS
