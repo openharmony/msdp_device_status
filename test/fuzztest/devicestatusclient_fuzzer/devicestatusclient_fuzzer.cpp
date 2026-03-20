9cfc8e3ae1f694b26b04d3d995e61d3e460359b8
use-your-head <yangyanling13@huawei.com>
2026-03-20 14:04:03 +0800

    use-your-head <yangyanling13@huawei.com> Co-Authored-By: Agent
    Signed-off-by: use-your-head <yangyanling13@huawei.com>

--- test/fuzztest/devicestatusclient_fuzzer/devicestatusclient_fuzzer.cpp
+++ test/fuzztest/devicestatusclient_fuzzer/devicestatusclient_fuzzer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -42,7 +42,6 @@ void DeviceStatusClientFuzzer::DeviceStatusTestCallback::OnDeviceStatusChanged(c
 {
     (void)devicestatusData.type;
     (void)devicestatusData.value;
-    return true;
 }
 
 void DeviceStatusClientFuzzer::TestSubscribeCallback(const uint8_t* data)
