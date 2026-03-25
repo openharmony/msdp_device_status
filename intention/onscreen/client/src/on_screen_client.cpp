954ce935b6cd0e30bd18e608e34041294474db24
chen_peng1 <chenpeng387@h-partners.com>
2026-03-25 10:14:17 +0800

    fix log
    Signed-off-by: chen_peng1 <chenpeng387@h-partners.com>

--- intention/onscreen/client/src/on_screen_client.cpp
+++ intention/onscreen/client/src/on_screen_client.cpp
@@ -72,11 +72,12 @@ int32_t OnScreenClient::IsParallelFeatureEnabled(int32_t windowId, int32_t& outS
     auto ret = INTENTION_CLIENT->IsParallelFeatureEnabled(windowId, outStatus);
     return ret;
 }
-
+//LCOV_EXCL_START
 int32_t OnScreenClient::GetLiveStatus()
 {
     return INTENTION_CLIENT->GetLiveStatus();
 }
+//LCOV_EXCL_STOP
 
 int32_t OnScreenClient::RegisterAwarenessCallback(const AwarenessCap& cap,
     const sptr<IRemoteOnScreenCallback>& callback, const AwarenessOptions& option)
