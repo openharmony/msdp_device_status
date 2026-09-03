57ac984ba4809bfab8e2c4b8925d7d0e78731da4
xing-jiangpan2 <xingjiangpan2@huawei.com>
2026-09-03 15:37:08 +0800

    Update summaryinfo for drag Co-Authored-By: Agent
    Signed-off-by: xing-jiangpan2 <xingjiangpan2@huawei.com>

--- utils/common/src/drag_data_packer.cpp
+++ utils/common/src/drag_data_packer.cpp
@@ -113,28 +113,20 @@ int32_t DragDataPacker::UnMarshallingDragAnimationType(Parcel &data, DragData &d
 
 int32_t DragDataPacker::MarshallingFilenameExtensions(const DragData &dragData, Parcel &data)
 {
-    std::vector<std::string> filenameExtensions = dragData.filenameExtensions;
-    if (filenameExtensions.empty()) {
-        FI_HILOGE("Invalid parameter filenameExtensions");
-        return RET_ERR;
-    }
-    int32_t fileNum = static_cast<int32_t>(filenameExtensions.size());
-    if (fileNum < 0) {
-        FI_HILOGE("fileNum overflow, size=%{public}d", fileNum);
-        return RET_ERR;
-    }
+    int32_t fileNum = static_cast<int32_t>(dragData.filenameExtensions.size());
     WRITEINT32(data, fileNum, E_DEVICESTATUS_WRITE_PARCEL_ERROR);
     for (int32_t i = 0; i < fileNum; i++) {
-        WRITESTRING(data, filenameExtensions[i], E_DEVICESTATUS_WRITE_PARCEL_ERROR);
+        WRITESTRING(data, dragData.filenameExtensions[i], E_DEVICESTATUS_WRITE_PARCEL_ERROR);
     }
     return RET_OK;
 }
- 
+
 int32_t DragDataPacker::UnMarshallingFilenameExtensions(Parcel &data, DragData &dragData)
 {
+    size_t readAbleSize = data.GetReadableBytes();
     int32_t fileNum = 0;
     READINT32(data, fileNum, E_DEVICESTATUS_READ_PARCEL_ERROR);
-    if ((fileNum <= 0) && (fileNum > MAX_BUF_SIZE)) {
+    if ((fileNum < 0) || (static_cast<size_t>(fileNum) > readAbleSize) || (fileNum > MAX_BUF_SIZE)) {
         FI_HILOGE("Invalid fileNum:%{public}d", fileNum);
         return RET_ERR;
     }
